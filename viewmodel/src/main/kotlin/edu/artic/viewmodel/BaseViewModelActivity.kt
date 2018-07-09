60b097d92a72e5961af2db571bad59458bfde9e7
Piotr Leja <pleja@fuzzproductions.com>
2018-07-09 15:09:55 -0400

    [Setup] add some base code for view model work

--- viewmodel/src/main/kotlin/edu/artic/viewmodel/BaseViewModelActivity.kt
+++ viewmodel/src/main/kotlin/edu/artic/viewmodel/BaseViewModelActivity.kt
@@ -2,13 +2,49 @@ package edu.artic.viewmodel
 
 import android.os.Bundle
 import edu.artic.ui.BaseActivity
+import javax.inject.Inject
 import kotlin.reflect.KClass
 
 abstract class BaseViewModelActivity<TViewModel : BaseViewModel> : BaseActivity() {
 
-    protected abstract val viewModelClass : KClass<TViewModel>
+    protected abstract val viewModelClass: KClass<TViewModel>
 
     open val useFactory: Boolean
-    get() = true
+        get() = true
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModelLazy: InvalidatableLazyImpl<TViewModel> = invalidatableLazy {
+        viewModelProvider(useFactory, viewModelFactory).get(viewModelClass.java)
+    }
+    val viewModel: TViewModel by viewModelLazy
+
+    val viewModelExists: Boolean
+        get() = viewModelLazy.isInitialized()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!viewModelExists) {
+            val viewModel = viewModel
+            onViewModelCreated(viewModel)
+        }
+
+    }
+
+    /**
+     * Called when [TViewModel] has been created.
+     */
+    protected open fun onViewModelCreated(viewModel: TViewModel) = Unit
+
+    override fun onDestroy() {
+        try {
+            if(viewModelExists) {
+                viewModel.cleanup()
+            }
+        } catch (i : IllegalStateException) {
+
+        }
+        super.onDestroy()
+    }
 }
--- viewmodel/src/main/kotlin/edu/artic/viewmodel/BaseViewModelFragment.kt
+++ viewmodel/src/main/kotlin/edu/artic/viewmodel/BaseViewModelFragment.kt
@@ -3,5 +3,9 @@ package edu.artic.viewmodel
 import edu.artic.ui.BaseFragment
 
 abstract class BaseViewModelFragment<TViewModel : BaseViewModel> : BaseFragment() {
-
+    /**
+    * If true we resolve our [ViewModel] instances via our [ViewModelFactory] class.
+    */
+    open val useFactory: Boolean
+        get() = true
 }
