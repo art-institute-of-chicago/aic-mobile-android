7f78f44c4b9ea80b3287701462bcbedfa28e1627
Piotr Leja <pleja@fuzzproductions.com>
2018-07-09 14:53:22 -0400

    [Splash] update base classes to allow for better view model stuff

--- app/src/main/kotlin/edu/artic/AppComponent.kt
+++ app/src/main/kotlin/edu/artic/AppComponent.kt
@@ -3,10 +3,12 @@ package edu.artic
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
+import edu.artic.viewmodel.ViewModelModule
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [
+    ViewModelModule::class,
     AndroidSupportInjectionModule::class
 ])
 interface AppComponent : AndroidInjector<ArticApp> {
