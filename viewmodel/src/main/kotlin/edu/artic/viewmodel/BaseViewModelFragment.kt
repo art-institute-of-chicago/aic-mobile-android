f77ae7494c04217a48f3e0a00575f498639e0f31
Piotr Leja <pleja@fuzzproductions.com>
2018-07-11 17:21:21 -0400

    Update BaseFragment and BaseViewModel fragments so BaseViewModelFragment actually works

--- viewmodel/src/main/kotlin/edu/artic/viewmodel/BaseViewModelFragment.kt
+++ viewmodel/src/main/kotlin/edu/artic/viewmodel/BaseViewModelFragment.kt
@@ -1,11 +1,73 @@
 package edu.artic.viewmodel
 
+import android.arch.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.view.View
 import edu.artic.ui.BaseFragment
+import javax.inject.Inject
+import kotlin.reflect.KClass
 
+/**
+ * Description: Provides common [ParentViewModel] methods for subclasses, so duplicate code doesn't
+ * happen.
+ */
 abstract class BaseViewModelFragment<TViewModel : BaseViewModel> : BaseFragment() {
+
+    protected abstract val viewModelClass: KClass<TViewModel>
     /**
-    * If true we resolve our [ViewModel] instances via our [ViewModelFactory] class.
-    */
+     * @return True by default if we use fragment for view model provider. Otherwise we use the [BaseActivity]
+     * * as where the [TViewModel] lives.
+     */
+    protected open fun useFragmentForProvider(): Boolean = true
+
+    /**
+     * If true we resolve our [ViewModel] instances via our [ViewModelFactory] class.
+     */
     open val useFactory: Boolean
         get() = true
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModelLazy: InvalidatableLazyImpl<TViewModel> = invalidatableLazy {
+        val viewModelProvider = if (useFragmentForProvider()) {
+            getViewModelProvider(viewModelFactory)
+        } else {
+            baseActivity.viewModelProvider(useFactory, viewModelFactory)
+        }
+        return@invalidatableLazy getViewModelForClass(viewModelProvider, viewModelClass)
+    }
+    val viewModel: TViewModel by viewModelLazy
+
+    val viewModelExists: Boolean
+        get() = viewModelLazy.isInitialized()
+
+    protected open fun getViewModelForClass(viewModelProvider: ViewModelProvider,
+                                            kClass: KClass<TViewModel>): TViewModel =
+            viewModelProvider.get(kClass.java)
+
+    protected open fun onViewModelCreated(viewModel : TViewModel) {
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (!viewModelExists) {
+            val viewModel = viewModel
+            onViewModelCreated(viewModel)
+        }
+    }
+
+    override fun onDestroyView() {
+        // attempt cleanup. if activity destroyed we will ignore this call here.
+        try {
+            if (viewModelExists) {
+                viewModel.cleanup()
+            }
+        } catch (i: IllegalStateException) {
+
+        }
+        super.onDestroyView()
+    }
+
 }
