6ce363a43b14d62ec27679efdaba4a9d4ad2e6c6
Piotr Leja <pleja@fuzzproductions.com>
2018-07-12 12:57:07 -0400

    [Blob Download] check headers bug work

--- db/src/main/kotlin/edu/artic/db/BlobApi.kt
+++ db/src/main/kotlin/edu/artic/db/BlobApi.kt
@@ -2,15 +2,19 @@ package edu.artic.db
 
 import com.jakewharton.retrofit2.adapter.rxjava2.Result
 import edu.artic.db.models.ArticBlobData
+import edu.artic.db.progress.Constants
 import io.reactivex.Observable
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.HEAD
+import retrofit2.http.Header
 
 interface BlobApi {
 
     @HEAD("appData-v2.json")
-    fun getBlobHeaders(): Observable<okhttp3.Response>
+    fun getBlobHeaders(): Observable<Result<Response<Void>>>
+
 
     @GET("appData-v2.json")
-    fun getBlob(): Observable<Result<ArticBlobData>>
+    fun getBlob(@Header(Constants.DOWNLOAD_IDENTIFIER_HEADER) header: String): Observable<Result<ArticBlobData>>
 }
--- db/src/main/kotlin/edu/artic/db/RetrofitBlobProvider.kt
+++ db/src/main/kotlin/edu/artic/db/RetrofitBlobProvider.kt
@@ -1,37 +1,50 @@
 package edu.artic.db
 
+import android.util.Log
+import edu.artic.db.progress.ProgressEventBus
 import io.reactivex.Observable
 import retrofit2.Retrofit
 import javax.inject.Named
 
-class RetrofitBlobProvider(@Named(ApiModule.RETROFIT_BLOB_API) retrofit: Retrofit) : BlobProvider {
+class RetrofitBlobProvider(@Named(ApiModule.RETROFIT_BLOB_API) retrofit: Retrofit, private val progressEventBus: ProgressEventBus) : BlobProvider {
+    companion object {
+        const val HEADER_ID = "blob_download_header_id"
+    }
 
     private val service = retrofit.create(BlobApi::class.java)
 
     override fun getBlob(): Observable<BlobState> {
         return Observable.create<BlobState> { observer ->
-
             //TODO: Create subsribe to some kind of progress listener and then send that to observer in BlobState.Downloading()
-
-            service.getBlob().subscribe({
-                observer.onNext(BlobState.Done(it))
-            }, {
-                observer.onError(it)
-            }, {
-                observer.onComplete()
-            })
+            progressEventBus.observable()
+                    .subscribe {
+                        if (it.downloadIdentifier == HEADER_ID) {
+                            observer.onNext(BlobState.Downloading(it.progress / 100f))
+                        }
+                    }
+            service.getBlob(HEADER_ID)
+                    .subscribe({
+                        observer.onNext(BlobState.Done(it))
+                    }, {
+                        observer.onError(it)
+                    }, {
+                        observer.onComplete()
+                    })
         }
     }
 
     override fun getBlobHeaders(): Observable<Map<String, List<String>>> {
         return Observable.create { observer ->
-            service.getBlobHeaders().subscribe({
-                observer.onNext(it.headers().toMultimap())
-            }, {
-                observer.onError(it)
-            }, {
-                observer.onComplete()
-            })
+            service.getBlobHeaders()
+                    .subscribe{
+                        Log.d("BlobProvider", "isError ${it.isError}")
+//                        observer.onNext(it.headers().toMultimap())
+                    }
+//            , {
+//                        observer.onError(it)
+//                    }, {
+//                        observer.onComplete()
+//                    })
 
         }
     }
