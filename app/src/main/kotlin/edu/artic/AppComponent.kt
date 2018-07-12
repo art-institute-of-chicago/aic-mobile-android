4e1794ed4efdb57aabb1f5ec6cbdd7134cd3c35b
Piotr Leja <pleja@fuzzproductions.com>
2018-07-12 11:27:28 -0400

    minor fixes to allow code to build properly

--- app/src/main/kotlin/edu/artic/AppComponent.kt
+++ app/src/main/kotlin/edu/artic/AppComponent.kt
@@ -3,6 +3,7 @@ package edu.artic
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
+import edu.artic.db.ApiModule
 import edu.artic.splash.SplashModule
 import edu.artic.viewmodel.ViewModelModule
 import javax.inject.Singleton
@@ -11,6 +12,7 @@ import javax.inject.Singleton
 @Component(modules = [
     SplashModule::class,
     ViewModelModule::class,
+    ApiModule::class,
     AndroidSupportInjectionModule::class
 ])
 interface AppComponent : AndroidInjector<ArticApp> {
