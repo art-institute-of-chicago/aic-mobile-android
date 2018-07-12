6cd2261f31e2a54a6e7ba403489ceb92f0869c14
Piotr Leja <pleja@fuzzproductions.com>
2018-07-12 12:29:20 -0400

    [Download] add code to allow downloading

--- db/src/main/kotlin/edu/artic/db/progress/ProgressEventBus.kt
+++ db/src/main/kotlin/edu/artic/db/progress/ProgressEventBus.kt
@@ -1,4 +1,18 @@
 package edu.artic.db.progress
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
+/**
+ * @author Jobin Lawrance
+ * @version 1.0.0
+ */
 class ProgressEventBus {
+
+    val mBusSubject: PublishSubject<ProgressEvent> = PublishSubject.create()
+
+    fun post(progressEvent: ProgressEvent) {
+        mBusSubject.onNext(progressEvent)
+    }
+
+    fun observable(): Observable<ProgressEvent> = mBusSubject
 }
