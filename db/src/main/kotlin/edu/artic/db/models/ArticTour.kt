b2bfa7726f56e8260e179dacd45b70f136394015
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-07-12 18:54:14 -0400

    [Tour] - Adds tour summary in home screen

--- db/src/main/kotlin/edu/artic/db/models/ArticTour.kt
+++ db/src/main/kotlin/edu/artic/db/models/ArticTour.kt
@@ -3,49 +3,59 @@ package edu.artic.db.models
 import com.squareup.moshi.Json
 
 data class ArticTour(
-val title : String,
-val status : String,
-val nid : String,
-val type : String,
-val translations : String,
-val location : String,
-val latitude : String,
-val longitude : String,
-val floor : String,
-val image_filename : String,
-val image_url : String,
-val image_filemime : String,
-val image_filesize : String,
-val image_width : String,
-val image_height : String,
+        @Json(name = "title") val title: String,
+        @Json(name = "status") val status: String,
+        @Json(name = "nid") val nid: String,
+        @Json(name = "type") val type: String,
+        @Json(name = "translations") val translations: List<Translation>,
+        @Json(name = "location") val location: String,
+        @Json(name = "latitude") val latitude: String,
+        @Json(name = "longitude") val longitude: String,
+        @Json(name = "floor") val floor: String,
+        @Json(name = "image_filename") val imageFileName: String,
+        @Json(name = "image_url") val imageUrl: String,
+        @Json(name = "image_filemime") val imageFileMime: String,
+        @Json(name = "image_filesize") val imageFileSize: String,
+        @Json(name = "image_width") val imageWidth: String,
+        @Json(name = "image_height") val imageHeight: String,
 //val thumbnail_crop_rect : [], // Unsure what type goes here so leaving removed for now
-val thumbnail_full_path : String,
+        @Json(name = "thumbnail_full_path") val thumbnailFullPath: String,
 //val large_image_crop_rect : [], // Unsure what type goes here so leaving removed for now
-val large_image_crop_v2 : CropRect,
-val large_image_full_path : String,
-val tour_banner : String ,
-val selector_number : String ,
-val description : String,
-val description_html : String,
-val tour_dates : TourDate,
-val intro : String,
-val intro_html : String,
-val tour_duration : String,
-val tour_audio : String,
-val category : Any, // TODO: figure out what this is
-val stops : List<Any>, //TODO: figure out what this is
-val weight : Int,
-val tour_stops: List<TourStop>
+        @Json(name = "large_image_crop_v2") val largeImageCropV2: CropRect,
+        @Json(name = "large_image_full_path") val largeImageFullPath: String,
+        @Json(name = "tour_banner") val tourBanner: String,
+        @Json(name = "selector_number") val selectorNumber: String,
+        @Json(name = "description") val description: String,
+        @Json(name = "description_html") val descriptionHtml: String,
+        @Json(name = "tour_dates") val tourDates: TourDate,
+        @Json(name = "intro") val intro: String,
+        @Json(name = "intro_html") val introHtml: String,
+        @Json(name = "tour_duration") val tourDuration: String,
+        @Json(name = "tour_audio") val tourAudio: String,
+        @Json(name = "category") val category: Any, // TODO: figure out what this is
+        @Json(name = "stops") val stops: List<Any>, //TODO: figure out what this is
+        @Json(name = "weight") val weight: Int,
+        @Json(name = "tour_stops") val tourStops: List<TourStop>?
 ) {
     data class TourDate(
-    val start_date : String,
-    val end_date : String
+            @Json(name = "start_date") val startDate: String,
+            @Json(name = "end_date") val endDate: String
     )
 
     data class TourStop(
-        @Json(name = "object") val objectId: String,
-        val audio_id : String,
-        val audio_bumper : String,
-        val sort : Int
-        )
+            @Json(name = "object") val objectId: String,
+            val audio_id: String,
+            val audio_bumper: String?,
+            val sort: Int
+    )
+
+    data class Translation(
+            @Json(name = "language") val language: String,
+            @Json(name = "title") val title: String,
+            @Json(name = "description") val description: String,
+            @Json(name = "description_html") val description_html: String,
+            @Json(name = "intro") val intro: String,
+            @Json(name = "intro_html") val intro_html: String,
+            @Json(name = "tour_duration") val tour_duration: String
+    )
 }
---
+++ welcome/src/main/kotlin/edu/artic/welcome/ToursAdapter.kt
@@ -0,0 +1,45 @@
+package edu.artic.welcome
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import edu.artic.db.models.ArticTour
+
+/**
+ * @author Sameer Dhakal (Fuzz)
+ */
+
+class ToursAdapter(val tours: List<ArticTour>, val context: Context) : RecyclerView.Adapter<ToursViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToursViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.tour_card_layout, parent, false)
+        return ToursViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return tours.count()
+    }
+
+    override fun onBindViewHolder(holder: ToursViewHolder, position: Int) {
+        val tour = tours[position]
+        holder.tourTitle.text = tour.title
+        holder.tourDescription.text = tour.description
+        val count = tour.tourStops?.count() ?: 0
+        holder.tourStopsCount.text = count.toString()
+        holder.tourTime.text = tour.tourDuration
+    }
+
+}
+
+
+class ToursViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val tourImage: ImageView = itemView.findViewById(R.id.image)
+    val tourTitle: TextView = itemView.findViewById(R.id.tourTitle)
+    val tourDescription: TextView = itemView.findViewById(R.id.tourDescription)
+    val tourStopsCount: TextView = itemView.findViewById(R.id.stops)
+    val tourTime: TextView = itemView.findViewById(R.id.tourTime)
+}
