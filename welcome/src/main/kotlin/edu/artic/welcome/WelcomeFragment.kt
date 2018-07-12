7b6719f9f6b915594bdd4c7ec8f3b428d639b954
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-07-12 13:28:47 -0400

    [AppBar] - Fixes animation for search icon

--- welcome/src/main/kotlin/edu/artic/welcome/WelcomeFragment.kt
+++ welcome/src/main/kotlin/edu/artic/welcome/WelcomeFragment.kt
@@ -1,8 +1,10 @@
 package edu.artic.welcome
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.view.View
 import edu.artic.viewmodel.BaseViewModelFragment
+import kotlinx.android.synthetic.main.app_bar_layout.view.*
 import kotlinx.android.synthetic.main.fragment_welcome.*
 import kotlin.reflect.KClass
 
@@ -16,10 +18,11 @@ class WelcomeFragment : BaseViewModelFragment<WelcomeViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        appBarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
-            val progress: Double = 1 - Math.abs(verticalOffset) / appBarLayout!!.totalScrollRange.toDouble()
-            searchIcon.background.alpha = (progress * 255).toInt()
-            flagIcon.drawable.alpha = (progress * 255).toInt()
+        val appBar = appBarLayout as AppBarLayout
+        appBar.addOnOffsetChangedListener { aBarLayout, verticalOffset ->
+            val progress: Double = 1 - Math.abs(verticalOffset) / aBarLayout!!.totalScrollRange.toDouble()
+            appBar.searchIcon.background.alpha = (progress * 255).toInt()
+            appBar.flagIcon.drawable.alpha = (progress * 255).toInt()
         }
     }
 }
