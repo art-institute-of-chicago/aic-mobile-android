1e8a693d723a99beed09f1369703bbe2f079148f
Piotr Leja <pleja@fuzzproductions.com>
2018-07-16 16:29:20 -0400

    [Refactor] code cleanup

--- db/src/main/kotlin/edu/artic/db/AppDataManager.kt
+++ db/src/main/kotlin/edu/artic/db/AppDataManager.kt
@@ -16,14 +16,14 @@ class AppDataManager @Inject constructor(
 
     fun getBlob(): Observable<AppDataState> {
         return serviceProvider.getBlobHeaders()
-                .flatMap {
+                .flatMap { headers ->
 
                     val dtf = DateTimeFormatterBuilder()
                             //Thu, 12 Jul 2018 16:56:12 GMT
                             .appendPattern("E, dd MMM yyyy HH:mm:ss O")
                             .toFormatter()
-                    if (it.containsKey(HEADER_LAST_MODIFIED)) {
-                        val lastModified = it[HEADER_LAST_MODIFIED]?.get(0)!!
+                    if (headers.containsKey(HEADER_LAST_MODIFIED)) {
+                        val lastModified = headers[HEADER_LAST_MODIFIED]?.get(0)!!
                         val newLocalDateTime = LocalDateTime.parse(lastModified, dtf)
                         val storedLastModified = appDataPreferencesManager.lastModified
                         if (storedLastModified.isNotEmpty() && newLocalDateTime.isAfter(LocalDateTime.parse(storedLastModified, dtf))) {
@@ -35,63 +35,69 @@ class AppDataManager @Inject constructor(
                         serviceProvider.getBlob()
                     }
 
-                }.flatMap {
-                    if (it is AppDataState.Done) {
+                }.flatMap { appDataState ->
+                    if (appDataState is AppDataState.Done) {
                         //Save last downloaded headers
-                        it.headers[HEADER_LAST_MODIFIED]?.let {
+                        appDataState.headers[HEADER_LAST_MODIFIED]?.let {
                             appDataPreferencesManager.lastModified = it[0]
                         }
 
+                        val result = appDataState.result
+
                         try {
-                            appDatabase.dashboardDao.setDashBoard(it.result.dashboard)
+                            appDatabase.dashboardDao.setDashBoard(result.dashboard)
                         } catch (e: Throwable) {
                             e.printStackTrace()
                         }
                         try {
-                            appDatabase.generalInfoDao.setGeneralInfo(it.result.generalInfo)
+                            appDatabase.generalInfoDao.setGeneralInfo(result.generalInfo)
                         } catch (e: Throwable) {
                             e.printStackTrace()
                         }
-                        if (it.result.galleries?.isNotEmpty() == true) {
+                        val galleries = result.galleries
+                        if (galleries?.isNotEmpty() == true) {
                             try {
-                                appDatabase.galleryDao.addGalleries(it.result.galleries.values.toList())
+                                appDatabase.galleryDao.addGalleries(galleries.values.toList())
+                            } catch (e: Throwable) {
+                                e.printStackTrace()
+                            }
+                        }
+                        val objects = result.objects
+                        if (objects?.isNotEmpty() == true) {
+                            try {
+                                appDatabase.objectDao.addObjects(objects.values.toList())
+                            } catch (e: Throwable) {
+                                e.printStackTrace()
+                            }
+                        }
+                        val audioFiles = result.audioFiles
+                        if (audioFiles?.isNotEmpty() == true) {
+                            try {
+                                appDatabase.audioFileDao.addAudioFiles(audioFiles.values.toList())
                             } catch (e: Throwable) {
                                 e.printStackTrace()
                             }
                         }
 
-                        if (it.result.objects?.isNotEmpty() == true) {
+                        val tours = result.tours
+                        if (tours?.isNotEmpty() == true) {
                             try {
-                                appDatabase.objectDao.addObjects(it.result.objects.values.toList())
+                                appDatabase.articTourDao.addTours(tours)
                             } catch (e: Throwable) {
                                 e.printStackTrace()
                             }
                         }
 
-                        if (it.result.audioFiles?.isNotEmpty() == true) {
+                        val mapAnnotations = result.mapAnnotations
+                        if (mapAnnotations?.isNotEmpty() == true) {
                             try {
-                                appDatabase.audioFileDao.addAudioFiles(it.result.audioFiles.values.toList())
-                            } catch (e: Throwable) {
-                                e.printStackTrace()
-                            }
-                        }
-
-                        if (it.result.tours?.isNotEmpty() == true) {
-                            try {
-                                appDatabase.articTourDao.addTours(it.result.tours)
-                            } catch (e: Throwable) {
-                                e.printStackTrace()
-                            }
-                        }
-                        if (it.result.mapAnnotations?.isNotEmpty() == true) {
-                            try {
-                                appDatabase.articMapAnnotationDao.addAnnotations(it.result.mapAnnotations.values.toList())
+                                appDatabase.articMapAnnotationDao.addAnnotations(mapAnnotations.values.toList())
                             } catch (e: Throwable) {
                                 e.printStackTrace()
                             }
                         }
                     }
-                    return@flatMap Observable.just(it)
+                    return@flatMap Observable.just(appDataState)
                 }
     }
 }
