94e133e493b0e057b836c2c44067babc53e18867
Piotr Leja <pleja@fuzzproductions.com>
2018-07-20 13:15:10 -0400

    [ViewAllExhibitions] add view model for AllExhibtions (AIC-25)(AIC-188)

---
+++ exhibitions/src/main/kotlin/edu/artic/exhibitions/AllExhibitionsViewModel.kt
@@ -0,0 +1,35 @@
+package edu.artic.exhibitions
+
+import com.fuzz.rx.bindTo
+import com.fuzz.rx.disposedBy
+import edu.artic.db.daos.ArticExhibitionDao
+import edu.artic.db.models.ArticExhibition
+import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+import javax.inject.Inject
+
+class AllExhibitionsViewModel @Inject constructor(exhibitionsDao: ArticExhibitionDao) : BaseViewModel() {
+
+    val exhibitions: Subject<List<AllExhibitionsCellViewModel>> = BehaviorSubject.create()
+
+    init {
+        exhibitionsDao.getAllExhibitions()
+                .map { list ->
+                    val viewModelList = ArrayList<AllExhibitionsCellViewModel>()
+                    list.forEach { exhibition ->
+                        viewModelList.add(AllExhibitionsCellViewModel(exhibition))
+                    }
+                    return@map viewModelList
+                }
+                .bindTo(exhibitions)
+                .disposedBy(disposeBag)
+    }
+
+}
+
+class AllExhibitionsCellViewModel(exhibition: ArticExhibition) : BaseViewModel() {
+    val exhibitionTitle: Subject<String> = BehaviorSubject.createDefault(exhibition.title)
+    val exhibitionDescription: Subject<String> = BehaviorSubject.createDefault("")
+    val exhibitionImageUrl: Subject<String> = BehaviorSubject.createDefault(exhibition.legacy_image_mobile_url)
+}
