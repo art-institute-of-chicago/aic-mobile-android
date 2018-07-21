34be55a18cdbb53df9505cb616979735e0a196aa
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-07-21 13:32:52 -0400

    [Navigation] - Adds navigation graphs for navigating

--- main/src/main/java/edu/artic/main/MainActivity.kt
+++ main/src/main/java/edu/artic/main/MainActivity.kt
@@ -1,17 +1,14 @@
 package edu.artic.main
 
 
-import android.os.Bundle
-import android.support.v4.app.FragmentTransaction
-import android.view.WindowManager
-import edu.artic.base.utils.disableShiftMode
-
-import edu.artic.viewmodel.BaseViewModelActivity
-import edu.artic.welcome.WelcomeFragment
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.reflect.KClass
 import android.app.Activity
 import android.graphics.Color
+import android.os.Bundle
+import android.view.WindowManager
+import edu.artic.base.utils.disableShiftMode
+import edu.artic.viewmodel.BaseViewModelActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.reflect.KClass
 
 
 /**
@@ -29,12 +26,11 @@ class MainActivity : BaseViewModelActivity<MainViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-        window.statusBarColor = Color.TRANSPARENT;
+        window.statusBarColor = Color.TRANSPARENT
         bottomNavigation.disableShiftMode(R.color.menu_color_list)
-        loadContainerFragment(WelcomeFragment())
     }
 
-    fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
+    private fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
         val win = activity.window
         val winParams = win.attributes
         if (on) {
@@ -45,16 +41,4 @@ class MainActivity : BaseViewModelActivity<MainViewModel>() {
         win.attributes = winParams
     }
 
-    /**
-     * Loads the provided @containerFragment into container
-     */
-    private fun loadContainerFragment(containerFragment: WelcomeFragment) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        if (container.childCount > 1) {
-            transaction.replace(R.id.container, containerFragment)
-        } else {
-            transaction.add(R.id.container, containerFragment)
-        }
-        transaction.commit()
-    }
 }
