0e951678aa163aa293a3ef302c235cf40e4f0c02
Piotr Leja <pleja@fuzzproductions.com>
2018-07-24 13:37:40 -0400

    [ExhibitionDetail] add transition animation from value home to detail (AIC-30)

--- exhibitions/src/main/kotlin/edu/artic/exhibitions/ExhibitionDetailFragment.kt
+++ exhibitions/src/main/kotlin/edu/artic/exhibitions/ExhibitionDetailFragment.kt
@@ -1,10 +1,18 @@
 package edu.artic.exhibitions
 
+import android.animation.ValueAnimator
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
+import android.support.transition.TransitionInflater
 import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.fuzz.rx.bindToMain
 import com.fuzz.rx.disposedBy
 import com.jakewharton.rxbinding2.view.clicks
@@ -29,14 +37,20 @@ class ExhibitionDetailFragment : BaseViewModelFragment<ExhibitionDetailViewModel
 
     private val exhibition by lazy { arguments!!.getParcelable<ArticExhibition>(ARG_EXHIBITION) }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        postponeEnterTransition()
+        sharedElementEnterTransition =
+                TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         appBarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
             val progress: Double = 1 - Math.abs(verticalOffset) / appBarLayout.totalScrollRange.toDouble()
-            Timber.d("progress: $progress")
             if (progress <= .5) {
                 val diff = .5f - progress.toFloat()
-                Timber.d("difference: $diff")
                 if (diff <= .25f) {
                     appBarLayout.toolbarTitle.alpha = 0f
                     appBarLayout.expandedTitle.alpha = 1 - (diff / .25f)
@@ -49,8 +63,8 @@ class ExhibitionDetailFragment : BaseViewModelFragment<ExhibitionDetailViewModel
                 appBarLayout.expandedTitle.alpha = 1f
             }
 
-//            appBarLayout.toolbarTitle.alpha =
         }
+        exhibitionImage.transitionName = exhibition.title
     }
 
     override fun onRegisterViewModel(viewModel: ExhibitionDetailViewModel) {
@@ -67,9 +81,55 @@ class ExhibitionDetailFragment : BaseViewModelFragment<ExhibitionDetailViewModel
 
         viewModel.imageUrl
                 .subscribe {
+                    val options = RequestOptions()
+                            .dontAnimate()
+                            .dontTransform()
                     Glide.with(this)
+//                            .asBitmap()
                             .load(it)
-                            .into(exhibitionImage)
+                            .apply(options)
+                            .listener(object : RequestListener<Drawable> {
+                                override fun onLoadFailed(e: GlideException?,
+                                                          model: Any?,
+                                                          target: Target<Drawable>?,
+                                                          isFirstResource: Boolean)
+                                        : Boolean {
+                                    startPostponedEnterTransition()
+                                    return false
+                                }
+
+                                override fun onResourceReady(
+                                        resource: Drawable?,
+                                        model: Any?,
+                                        target: Target<Drawable>?,
+                                        dataSource: DataSource?,
+                                        isFirstResource: Boolean): Boolean {
+                                    resource?.let {
+                                        // Adds a nice animator to scale the container down to proper aspect ratio
+                                        val parentWidth = (exhibitionImage.parent as View).width
+                                        val newHeight = if (it.intrinsicWidth > it.intrinsicHeight) {
+                                            ((it.intrinsicHeight.toFloat() / it.intrinsicWidth.toFloat()) * parentWidth).toInt()
+                                        } else {
+                                            parentWidth
+                                        }
+                                        if (newHeight != parentWidth) {
+                                            val animator = ValueAnimator.ofInt(parentWidth, newHeight)
+                                            animator.addUpdateListener {
+                                                val params = exhibitionImage.layoutParams
+                                                params.width = parentWidth
+                                                params.height = it.animatedValue as Int
+                                                exhibitionImage.layoutParams = params
+                                            }
+                                            animator.duration = 500
+                                            animator.start()
+                                        }
+
+                                    }
+                                    startPostponedEnterTransition()
+                                    return false
+                                }
+
+                            }).into(exhibitionImage)
                 }
                 .disposedBy(disposeBag)
 
