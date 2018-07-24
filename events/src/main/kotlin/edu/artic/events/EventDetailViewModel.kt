ca5d8d498dde535af817433aebafd170ed00c07b
Piotr Leja <pleja@fuzzproductions.com>
2018-07-24 15:18:42 -0400

    [EventDetail] create view model for event detail fragment (AIC-33)(AIC-201)

---
+++ events/src/main/kotlin/edu/artic/events/EventDetailViewModel.kt
@@ -0,0 +1,58 @@
+package edu.artic.events
+
+import com.fuzz.rx.bindTo
+import com.fuzz.rx.disposedBy
+import edu.artic.base.utils.DateTimeHelper
+import edu.artic.db.models.ArticEvent
+import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+
+class EventDetailViewModel : BaseViewModel() {
+
+    val imageUrl: Subject<String> = BehaviorSubject.create()
+    val title: Subject<String> = BehaviorSubject.createDefault("test")
+    val metaData: Subject<String> = BehaviorSubject.createDefault("")
+    val description: Subject<String> = BehaviorSubject.createDefault("")
+    val throughDate: Subject<String> = BehaviorSubject.createDefault("")
+    val location: Subject<String> = BehaviorSubject.createDefault("")
+
+    var event: ArticEvent? = null
+        set(value) {
+            field = value
+            val eventObservable = BehaviorSubject.createDefault(value)
+
+            eventObservable
+                    .map { it.title }
+                    .bindTo(title)
+                    .disposedBy(disposeBag)
+
+            eventObservable
+                    .filter { it.image != null }
+                    .map { it.image!! }
+                    .bindTo(imageUrl)
+                    .disposedBy(disposeBag)
+
+            eventObservable
+                    .map { it.start_at.format(DateTimeHelper.HOME_EVENT_DATE_FORMATTER) }
+                    .bindTo(metaData)
+                    .disposedBy(disposeBag)
+
+            eventObservable
+                    .filter { it.description != null }
+                    .map { it.description!! }
+                    .bindTo(description)
+                    .disposedBy(disposeBag)
+
+            eventObservable
+                    .map { "Through ${it.end_at.format(DateTimeHelper.HOME_EXHIBITION_DATE_FORMATTER)}" }
+                    .bindTo(throughDate)
+                    .disposedBy(disposeBag)
+
+            eventObservable
+                    .filter { it.location != null }
+                    .map { it.location!! }
+                    .bindTo(location)
+                    .disposedBy(disposeBag)
+        }
+}
