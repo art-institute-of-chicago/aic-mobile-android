bda003cdf67773fe0e85557741f1950d066eaed9
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-07-24 14:43:35 -0400

    [Navigation] - Finishes the app if back pressed in home screen

--- welcome/src/main/kotlin/edu/artic/welcome/WelcomeActivity.kt
+++ welcome/src/main/kotlin/edu/artic/welcome/WelcomeActivity.kt
@@ -1,6 +1,8 @@
 package edu.artic.welcome
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.Navigation
 import edu.artic.base.utils.NavigationSelectListener
 import edu.artic.base.utils.disableShiftMode
 import edu.artic.ui.BaseActivity
@@ -8,14 +10,35 @@ import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : BaseActivity() {
 
-
     override val layoutResId: Int
         get() = R.layout.activity_welcome
 
+    companion object {
+        val EXRTA_QUIT: String = "Quit"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (intent?.extras?.getBoolean(EXRTA_QUIT) == true) {
+            finish()
+            return
+        }
         bottomNavigation.disableShiftMode(R.color.menu_color_list)
         bottomNavigation.selectedItemId = R.id.action_home
         bottomNavigation.setOnNavigationItemSelectedListener(NavigationSelectListener(this))
+    }
+
+    override fun onBackPressed() {
+        if (!isTaskRoot && supportFragmentManager.backStackEntryCount == 0) {
+            val navigationController = Navigation.findNavController(this, R.id.container)
+            if (navigationController.currentDestination.id == R.id.welcomeFragment) {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                intent.putExtra(EXRTA_QUIT, true)
+                startActivity(intent)
+                return
+            }
+        }
+        super.onBackPressed()
     }
 }
