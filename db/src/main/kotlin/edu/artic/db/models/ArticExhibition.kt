3efb0f9fd7bef2b00cab7177f3675d45b1f00b5e
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-07-25 13:15:42 -0400

    [Exhibition Image] - Uses exhibition image url from cms whenever available

--- db/src/main/kotlin/edu/artic/db/models/ArticExhibition.kt
+++ db/src/main/kotlin/edu/artic/db/models/ArticExhibition.kt
@@ -12,7 +12,7 @@ import org.threeten.bp.LocalDateTime
 data class ArticExhibition(
         @Json(name = "short_description") val short_description: String?,
         @Json(name = "aic_start_at") val aic_start_at: LocalDateTime,
-        @Json(name = "legacy_image_mobile_url") val legacy_image_mobile_url: String?,
+        @Json(name = "legacy_image_mobile_url") var legacy_image_mobile_url: String?,
         @Json(name = "legacy_image_desktop_url") val legacy_image_desktop_url: String?,
         @Json(name = "web_url") val web_url: String?,
         @Json(name = "gallery_id") val gallery_id: String?,
