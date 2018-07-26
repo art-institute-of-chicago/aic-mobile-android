00c5297302d40912c488b5de9415847c3c0ec57d
Piotr Leja <pleja@fuzzproductions.com>
2018-07-26 16:52:33 -0400

    [TourDetails] fix missing text etc in tour details first cell

--- db/src/main/kotlin/edu/artic/db/daos/ArticObjectDao.kt
+++ db/src/main/kotlin/edu/artic/db/daos/ArticObjectDao.kt
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import edu.artic.db.models.ArticObject
-import io.reactivex.Maybe
+import io.reactivex.Flowable
 import io.reactivex.Single
 
 @Dao
@@ -17,8 +17,9 @@ interface ArticObjectDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addObjects(objects: List<ArticObject>)
 
+    //Must be flowable due to need to use this in 2 places
     @Query("select * from ArticObject where nid = :id")
-    fun getObjectById(id: String): Single<ArticObject>
+    fun getObjectById(id: String): Flowable<ArticObject>
 
     @Query("select * from ArticObject where objectSelectorNumber = :selectorNumber")
     fun getObjectBySelectorNumber(selectorNumber: String): Single<ArticObject>
