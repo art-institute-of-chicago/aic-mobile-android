d3e78b2aef9b34e51ebe78771e195d24efb17b8e
Piotr Leja <pleja@fuzzproductions.com>
2018-07-26 12:38:51 -0400

    [TourDetails] some connections between fragment and view model (AIC-27)(AIC-207)(208)(211)

--- db/src/main/kotlin/edu/artic/db/daos/ArticObjectDao.kt
+++ db/src/main/kotlin/edu/artic/db/daos/ArticObjectDao.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import edu.artic.db.models.ArticObject
+import io.reactivex.Observable
 
 @Dao
 interface ArticObjectDao {
@@ -14,5 +15,8 @@ interface ArticObjectDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addObjects(objects: List<ArticObject>)
+
+    @Query("select * from ArticObject where nid = :id")
+    fun getObjectById(id: String) : Observable<ArticObject>
 
 }
