c2ebe1cfb4701d093b63688e822f4a2c1df9dcd0
Piotr Leja <pleja@fuzzproductions.com>
2018-07-31 13:48:47 -0400

    [Map] add map activity to handle all map related work

---
+++ map/src/main/kotlin/edu/artic/map/MapActivity.kt
@@ -0,0 +1,22 @@
+package edu.artic.map
+
+import android.os.Bundle
+import edu.artic.navigation.NavigationSelectListener
+import edu.artic.base.utils.disableShiftMode
+import edu.artic.ui.BaseActivity
+import kotlinx.android.synthetic.main.activity_map.*
+
+class MapActivity : BaseActivity() {
+
+    override val layoutResId: Int
+        get() = R.layout.activity_map
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bottomNavigation.apply {
+            disableShiftMode(R.color.map_menu_color_list)
+            selectedItemId = R.id.action_info
+            setOnNavigationItemSelectedListener(NavigationSelectListener(this.context))
+        }
+    }
+}
