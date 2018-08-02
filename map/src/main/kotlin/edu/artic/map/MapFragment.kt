5016b7d226e60268c5dad4c3cde4af6dfcb103cc
Piotr Leja <pleja@fuzzproductions.com>
2018-08-02 13:32:11 -0400

    [Map] add map (AIC-51)

--- map/src/main/kotlin/edu/artic/map/MapFragment.kt
+++ map/src/main/kotlin/edu/artic/map/MapFragment.kt
@@ -1,7 +1,13 @@
 package edu.artic.map
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import edu.artic.analytics.ScreenCategoryName
 import edu.artic.viewmodel.BaseViewModelFragment
+import kotlinx.android.synthetic.main.fragment_map.*
 import kotlin.reflect.KClass
 
 class MapFragment : BaseViewModelFragment<MapViewModel>() {
@@ -14,4 +20,56 @@ class MapFragment : BaseViewModelFragment<MapViewModel>() {
         get() = R.layout.fragment_map
     override val screenCategory: ScreenCategoryName
         get() = ScreenCategoryName.Map
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        MapsInitializer.initialize(view.context)
+        mapView.getMapAsync { map ->
+            map.setMinZoomPreference(18f)
+            map.setMaxZoomPreference(21f)
+            map.setLatLngBoundsForCameraTarget(
+                    LatLngBounds(
+
+                            LatLng(41.878523, -87.623689),
+                            LatLng(41.880712, -87.621100)
+                    )
+            )
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
