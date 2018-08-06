e27f5af5493a65163d242423797145ca2a1d1f8f
Piotr Leja <pleja@fuzzproductions.com>
2018-08-06 13:50:16 -0400

    [ZoomLevel] add loading of markers for zooms levels 1 and 2 based on floor (AIC-51)

--- map/src/main/kotlin/edu/artic/map/MapFragment.kt
+++ map/src/main/kotlin/edu/artic/map/MapFragment.kt
@@ -2,12 +2,19 @@ package edu.artic.map
 
 import android.os.Bundle
 import android.view.View
+import com.fuzz.rx.disposedBy
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.artic.analytics.ScreenCategoryName
 import edu.artic.viewmodel.BaseViewModelFragment
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.fragment_map.*
+import timber.log.Timber
 import kotlin.reflect.KClass
 
 class MapFragment : BaseViewModelFragment<MapViewModel>() {
@@ -20,12 +27,18 @@ class MapFragment : BaseViewModelFragment<MapViewModel>() {
         get() = R.layout.fragment_map
     override val screenCategory: ScreenCategoryName
         get() = ScreenCategoryName.Map
+    lateinit var destroyableMapView: MapView
+    lateinit var map: GoogleMap
+
+    val currentMarkers = mutableListOf<Marker>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        destroyableMapView = mapView
         mapView.onCreate(savedInstanceState)
         MapsInitializer.initialize(view.context)
         mapView.getMapAsync { map ->
+            this.map = map
             map.setMinZoomPreference(18f)
             map.setMaxZoomPreference(21f)
             map.setLatLngBoundsForCameraTarget(
@@ -35,8 +48,57 @@ class MapFragment : BaseViewModelFragment<MapViewModel>() {
                             LatLng(41.880712, -87.621100)
                     )
             )
+            map.setOnCameraIdleListener {
+                val zoom = map.cameraPosition.zoom
+                Timber.d("MapZoomLevel: $zoom")
+                when {
+                    zoom < 18.5 -> {
+                        viewModel.zoomLevelChangedTo(MapZoomLevel.One)
+                    }
+                    zoom < 20 -> {
+                        viewModel.zoomLevelChangedTo(MapZoomLevel.Two)
+                    }
+                    else -> {
+                        viewModel.zoomLevelChangedTo(MapZoomLevel.Three)
+                    }
+                }
+            }
         }
     }
+
+    override fun setupBindings(viewModel: MapViewModel) {
+        viewModel.mapAnnotations
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    currentMarkers.forEach {
+                        it.remove()
+                    }
+                    currentMarkers.clear()
+
+                    it.forEach { mapItem ->
+                        when (mapItem) {
+                            is MapItem.MapAnnotation -> {
+                                val annotation = mapItem.annotation
+                                currentMarkers.add(
+                                        map.addMarker(
+                                                MarkerOptions()
+                                                        .title(annotation.label)
+                                                        .position(
+                                                                LatLng(
+                                                                        annotation.latitude!!.toDouble(),
+                                                                        annotation.longitude!!.toDouble()
+                                                                )
+                                                        )
+                                        )
+                                )
+                            }
+                        }
+
+
+                    }
+                }.disposedBy(disposeBag)
+    }
+
 
     override fun onStart() {
         super.onStart()
@@ -59,8 +121,8 @@ class MapFragment : BaseViewModelFragment<MapViewModel>() {
     }
 
     override fun onDestroy() {
+        destroyableMapView.onDestroy()
         super.onDestroy()
-        mapView.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
---
+++ map/src/main/kotlin/edu/artic/map/MapZoomLevel.kt
@@ -0,0 +1,9 @@
+package edu.artic.map
+
+sealed class MapZoomLevel {
+
+    object One : MapZoomLevel()
+    object Two : MapZoomLevel()
+    object Three : MapZoomLevel()
+
+}
