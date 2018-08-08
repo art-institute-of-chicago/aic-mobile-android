537317dfed2d1a6d5ee76a6cdc25e87765f4695a
Piotr Leja <pleja@fuzzproductions.com>
2018-08-08 19:19:41 -0400

    [Refactor] fixed marker mess up and inline some marker creation

--- db/src/main/kotlin/edu/artic/db/daos/ArticMapAnnotationDao.kt
+++ db/src/main/kotlin/edu/artic/db/daos/ArticMapAnnotationDao.kt
@@ -26,6 +26,9 @@ abstract class ArticMapAnnotationDao {
     @Query("select * from ArticMapAnnotation where annotationType = \"Text\" and textType = :type and floor = :floor")
     abstract fun getTextAnnotationByTypeAndFloor(type: String, floor: String): Flowable<List<ArticMapAnnotation>>
 
+    @Query("select * from ArticMapAnnotation where annotationType = :annotationType and textType = :type ")
+    abstract fun getTextAnnotationByType(type: String, annotationType: String = ArticMapAnnotationType.TEXT): Flowable<List<ArticMapAnnotation>>
+
     fun getBuildingNamesOnMap(): Flowable<List<ArticMapAnnotation>> {
         return getAnnotationByType("Text")
     }
