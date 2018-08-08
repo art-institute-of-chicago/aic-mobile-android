240a483380211eb3a038d9d14f4040962500fce7
Piotr Leja <pleja@fuzzproductions.com>
2018-08-08 12:21:56 -0400

    [Map] refactor functionality to not have to regenerate markers at each zoom level

--- db/src/main/kotlin/edu/artic/db/daos/ArticMapAnnotationDao.kt
+++ db/src/main/kotlin/edu/artic/db/daos/ArticMapAnnotationDao.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import edu.artic.db.models.ArticMapAnnotation
+import edu.artic.db.models.ArticMapAnnotationType
 import io.reactivex.Flowable
 
 @Dao
@@ -16,8 +17,8 @@ abstract class ArticMapAnnotationDao {
     @Query("select * from ArticMapAnnotation where annotationType = :type")
     abstract fun getAnnotationByType(type: String): Flowable<List<ArticMapAnnotation>>
 
-    @Query("select * from ArticMapAnnotation where annotationType = \"Amenity\" and floor = :floor")
-    abstract fun getAmenitiesOnMapForFloor(floor : String): Flowable<List<ArticMapAnnotation>>
+    @Query("select * from ArticMapAnnotation where annotationType = :type and floor = :floor")
+    abstract fun getAnnotationByTypeForFloor(type: String, floor: String): Flowable<List<ArticMapAnnotation>>
 
     @Query("select * from ArticMapAnnotation where annotationType = \"Text\" and textType = :type ")
     abstract fun getTextAnnotationByType(type: String): Flowable<List<ArticMapAnnotation>>
@@ -29,11 +30,11 @@ abstract class ArticMapAnnotationDao {
         return getAnnotationByType("Text")
     }
 
-    fun getAmenitiesOnMap() : Flowable<List<ArticMapAnnotation>> {
-        return getAnnotationByType("Amenity")
+    fun getAmenitiesOnMapForFloor(floor: String): Flowable<List<ArticMapAnnotation>> {
+        return getAnnotationByTypeForFloor("Amenity", floor)
     }
 
-    fun getDepartmentOnMap() : Flowable<List<ArticMapAnnotation>> {
-        return getAnnotationByType("Department")
+    fun getDepartmentOnMapForFloor(floor: String): Flowable<List<ArticMapAnnotation>> {
+        return getAnnotationByTypeForFloor(ArticMapAnnotationType.DEPARTMENT, floor)
     }
 }
