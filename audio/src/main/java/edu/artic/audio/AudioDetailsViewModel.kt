5628e1d017902eeeea4ecc1114520a94137838e9
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-08-09 15:55:19 -0400

    [Refactor] - Refactors the audio details fragment to match architecture

--- audio/src/main/java/edu/artic/audio/AudioDetailsViewModel.kt
+++ audio/src/main/java/edu/artic/audio/AudioDetailsViewModel.kt
@@ -1,11 +1,67 @@
 package edu.artic.audio
 
+import com.fuzz.rx.bindTo
+import com.fuzz.rx.disposedBy
+import edu.artic.db.models.ArticObject
 import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
 /**
  * @author Sameer Dhakal (Fuzz)
  */
 class AudioDetailsViewModel @Inject constructor() : BaseViewModel() {
+    val title: Subject<String> = BehaviorSubject.create()
+    val image: Subject<String> = BehaviorSubject.create()
+    val transcript: Subject<String> = BehaviorSubject.create()
+    val credits: Subject<String> = BehaviorSubject.create()
+    val authorCulturalPlace: Subject<String> = BehaviorSubject.create()
+
+    private val objectObservable: Subject<ArticObject> = BehaviorSubject.create()
+
+    var audioObject: ArticObject? = null
+        set(value) {
+            field = value
+            value?.let {
+                objectObservable.onNext(it)
+            }
+        }
+
+    init {
+        objectObservable
+                .map {
+                    it.artistCulturePlaceDelim?.replace("\r", "\n") ?: ""
+                }.bindTo(authorCulturalPlace)
+                .disposedBy(disposeBag)
+
+        objectObservable
+                .map {
+                    it.title ?: ""
+                }.bindTo(title)
+                .disposedBy(disposeBag)
+
+
+        objectObservable
+                .map {
+                    it.largeImageFullPath ?: ""
+                }.bindTo(image)
+                .disposedBy(disposeBag)
+
+
+        objectObservable
+                .map {
+                    it.audioCommentary.first().audioFile?.transcript ?: ""
+                }.bindTo(transcript)
+                .disposedBy(disposeBag)
+
+        objectObservable
+                .map {
+                    it.audioCommentary.first().audioFile?.credits ?: ""
+                }.bindTo(credits)
+                .disposedBy(disposeBag)
+
+
+    }
 
 }
---
+++ map/src/main/kotlin/edu/artic/map/MapObjectDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package edu.artic.map
+
+/**
+@author Sameer Dhakal (Fuzz)
+ */
+class MapObjectDetailsVIewModel {
+}
