5592c7058789696978bc107d0481f2dd2ff5d96d
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-14 11:46:40 -0400

    [Refactor] - rename FileExtensions.kt to AssetsExtensions.kt

--- base/src/main/java/edu/artic/base/utils/AssetsExtensions.kt
+++ base/src/main/java/edu/artic/base/utils/AssetsExtensions.kt
@@ -4,6 +4,10 @@ import android.content.res.AssetManager
 import java.nio.charset.Charset
 
 /**
+ * Extension functions for working with [AssetManager].
+ *
+ * Loading files, defining resources, IO stuff like that.
+ *
  * @author Sameer Dhakal (Fuzz)
  */
 fun AssetManager.fileAsString(subdirectory: String, filename: String): String {
