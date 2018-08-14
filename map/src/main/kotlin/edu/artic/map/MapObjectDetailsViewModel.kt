a879f463709e041a36362e5330b63e910beed2f4
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-08-14 10:40:55 -0400

    [Analytics] - Implements map object audio played event

--- map/src/main/kotlin/edu/artic/map/MapObjectDetailsViewModel.kt
+++ map/src/main/kotlin/edu/artic/map/MapObjectDetailsViewModel.kt
@@ -1,10 +1,17 @@
 package edu.artic.map
 
+import com.fuzz.rx.Optional
 import com.fuzz.rx.bindTo
 import com.fuzz.rx.disposedBy
+import com.fuzz.rx.filterFlatMap
+import edu.artic.analytics.AnalyticsAction
+import edu.artic.analytics.AnalyticsTracker
+import edu.artic.analytics.EventCategoryName
+import edu.artic.db.models.ArticAudioFile
 import edu.artic.db.models.ArticObject
 import edu.artic.media.audio.AudioPlayerService
 import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.rxkotlin.withLatestFrom
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
 import javax.inject.Inject
@@ -12,7 +19,7 @@ import javax.inject.Inject
 /**
  * @author Sameer Dhakal (Fuzz)
  */
-class MapObjectDetailsViewModel @Inject constructor() : BaseViewModel() {
+class MapObjectDetailsViewModel @Inject constructor(val analyticsTracker: AnalyticsTracker) : BaseViewModel() {
 
     val title: Subject<String> = BehaviorSubject.create()
     val image: Subject<String> = BehaviorSubject.create()
@@ -21,6 +28,14 @@ class MapObjectDetailsViewModel @Inject constructor() : BaseViewModel() {
     private val objectObservable: Subject<ArticObject> = BehaviorSubject.create()
     val playState: Subject<AudioPlayerService.PlayBackState> = BehaviorSubject.create()
     val audioPlayBackStatus: Subject<AudioPlayerService.PlayBackState> = BehaviorSubject.create()
+
+    val currentTrack: Subject<Optional<ArticAudioFile>> = BehaviorSubject.createDefault(Optional(null))
+    val playerControl: Subject<PlayerAction> = BehaviorSubject.create()
+
+    sealed class PlayerAction {
+        class Play(val requestedObject: ArticObject) : PlayerAction()
+        class Pause : PlayerAction()
+    }
 
 
     var articObject: ArticObject? = null
@@ -59,6 +74,30 @@ class MapObjectDetailsViewModel @Inject constructor() : BaseViewModel() {
                     it.largeImageFullPath.orEmpty()
                 }.bindTo(image)
                 .disposedBy(disposeBag)
+
+
+        playerControl
+                .filterFlatMap({ it is PlayerAction.Play }, { it as PlayerAction.Play })
+                .withLatestFrom(currentTrack, objectObservable) { playerAction, currentTrack, articObject ->
+                    val requestedObject = playerAction.requestedObject.audioCommentary.first().audioFile
+                    (currentTrack.value != requestedObject) to articObject
+                }
+                .filter { (newTrack, _) -> newTrack }
+                .subscribe { (_, articObject) ->
+                    analyticsTracker.reportEvent(EventCategoryName.PlayAudio, AnalyticsAction.playAudioMap, articObject.title)
+                }.disposedBy(disposeBag)
+
+    }
+
+
+    fun playCurrentObject() {
+        articObject?.let {
+            playerControl.onNext(PlayerAction.Play(it))
+        }
+    }
+
+    fun pauseCurrentObject() {
+        playerControl.onNext(PlayerAction.Pause())
     }
 
 }
