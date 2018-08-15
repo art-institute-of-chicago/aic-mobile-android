239e07616b280c7bb107f565488bf303b25519b6
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-15 19:34:39 -0400

    [Documentation] - Explain purpose of 'GlideModule' class
    Also suppresses the 'unused' lint check

--- welcome/src/main/kotlin/edu/artic/welcome/GlideModule.kt
+++ welcome/src/main/kotlin/edu/artic/welcome/GlideModule.kt
@@ -9,9 +9,20 @@ import com.bumptech.glide.request.RequestOptions
 
 
 /**
+ * This is the app's implementation of [our image-loading library's contract][AppGlideModule].
+ *
+ * Whenever code needs to load an image from a remote server, it should do so by
+ * calling [one of the Glide.with() overloads][com.bumptech.glide.Glide.with]. This
+ * file defines default values for those loading operations via [applyOptions].
+ *
+ * Note: the [GlideModule] annotation on this class is used by Glide's annotation
+ * processor during the build. There is no need for other source files to reference
+ * this file.
+ *
  * @author Sameer Dhakal (Fuzz)
  */
 @GlideModule
+@Suppress("unused")
 class GlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context?, builder: GlideBuilder?) {
