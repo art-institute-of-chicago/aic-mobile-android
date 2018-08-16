42a1dc98633017441426cbe37c80ba7f43e10a62
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-16 10:48:27 -0400

    [Images] - GlideModule: prefer memory-aware downsampling

--- welcome/src/main/kotlin/edu/artic/welcome/GlideModule.kt
+++ welcome/src/main/kotlin/edu/artic/welcome/GlideModule.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.SimpleTarget
 
 
 /**
@@ -27,12 +29,44 @@ class GlideModule : AppGlideModule() {
 
     // NB: 'appcontext' and 'builder' are guaranteed non-null, based on analysis of sources for Glide 4.4.0
     override fun applyOptions(appContext: Context, builder: GlideBuilder) {
-        super.applyOptions(appContext, builder)
-        builder.setDefaultRequestOptions(RequestOptions().format(DecodeFormat.PREFER_ARGB_8888))
+        builder.setDefaultRequestOptions(
+                RequestOptions()
+                        .format(DecodeFormat.PREFER_ARGB_8888)
+                        .downsample(MemoryOptimizedDownsampleStrategy)
+        )
 
     }
 
     override fun isManifestParsingEnabled(): Boolean {
         return false
+    }
+}
+
+
+/**
+ * A simple memory-aware strategy based on 'center outside'.
+ *
+ * Note this strategy only comes into effect if the source image and target
+ * ImageView are different sizes. To change the resolution of properly-sized
+ * resources, you'll need to use a new [DownsampleStrategy] and implement
+ * [DownsampleStrategy.getScaleFactor] accordingly.
+ *
+ * [MemoryOptimizedDownsampleStrategy.getScaleFactor] simply calls the method
+ * of the same name on [CENTER_OUTSIDE][DownsampleStrategy.CENTER_OUTSIDE]. The
+ * intended effect is for each image to be cropped in the larger dimension, so
+ * that every part of the [load target][SimpleTarget] is covered by some part of
+ * the image.
+ *
+ * As of Glide 4.4, the [default strategy][DownsampleStrategy.DEFAULT] was set to
+ * `CENTER_OUTSIDE` anyway, so our only distinction in that version is the use of
+ * [DownsampleStrategy.SampleSizeRounding.MEMORY].
+ */
+object MemoryOptimizedDownsampleStrategy : DownsampleStrategy() {
+    override fun getScaleFactor(sourceWidth: Int, sourceHeight: Int, requestedWidth: Int, requestedHeight: Int): Float {
+        return DownsampleStrategy.CENTER_OUTSIDE.getScaleFactor(sourceWidth, sourceHeight, requestedWidth, requestedHeight)
+    }
+
+    override fun getSampleSizeRounding(sourceWidth: Int, sourceHeight: Int, requestedWidth: Int, requestedHeight: Int): SampleSizeRounding {
+        return SampleSizeRounding.MEMORY
     }
 }
