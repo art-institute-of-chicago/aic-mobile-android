13abeaf4e7bdb4d196ba566685c4e64f77026e64
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-17 20:13:13 -0400

    [Language] - introduce a restricted-selection method to LanguageSelector
    This can be used to figure out which of a set of translations is closest
    to the current

--- localization/src/main/kotlin/edu/artic/localization/LanguageSelector.kt
+++ localization/src/main/kotlin/edu/artic/localization/LanguageSelector.kt
@@ -1,17 +1,19 @@
 package edu.artic.localization
 
-import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
 import java.util.*
+import java.util.concurrent.atomic.AtomicReference
 
 /**
- * This class hosts one or more [RX subjects][Subject] related to language.
+ * This class hosts zero or more [RX subjects][Subject] related to language.
  *
  * Use this instead of going directly to [the default Locale][java.util.Locale.getDefault].
+ *
+ * @see BaseTranslation
  */
 class LanguageSelector(private val prefs: LocalizationPreferences) {
 
-    val appLocaleObservable: Subject<Locale> = BehaviorSubject.createDefault(prefs.preferredAppLocale)
+    private val appLocaleRef: AtomicReference<Locale> = AtomicReference(prefs.preferredAppLocale)
 
     fun setDefaultLanguageForApplication(lang: Locale) {
         if (lang.hasNoLanguage()) {
@@ -20,8 +22,27 @@ class LanguageSelector(private val prefs: LocalizationPreferences) {
             }
         } else {
             prefs.preferredAppLocale = lang
-            appLocaleObservable.onNext(lang)
+            appLocaleRef.set(lang)
         }
+    }
+
+    /**
+     * Returns the first translation we can find in `languages` that belongs to the
+     * same locale as [appLocaleRef]. If no such exists, we return the first language
+     * in the list.
+     *
+     * As per the requirements laid out in [Locale.getLanguage], we only convert
+     * languages that have been normalized through [Locale.forLanguageTag]. We
+     * cannot necessarily trust the original Strings returned by the API.
+     */
+    fun <T : BaseTranslation> selectFrom(languages: List<T>): T {
+        val appLocale: Locale = appLocaleRef.get()
+
+        // TODO: Investigate possibility of replacing the list with a `LocaleList`
+        return languages.firstOrNull {
+            // This is very much intentional. Read the method docs fully before changing.
+            it.underlyingLocale().language == appLocale.language
+        } ?: languages[0]
     }
 
 }
