540342d71110eacaa7f041bb1ba4a08a296c06b5
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-08-21 17:30:57 -0400

    [Start Tour] - Tour : Navigates user to map on start tour button click

--- tours/src/main/kotlin/edu/artic/tours/TourDetailsFragment.kt
+++ tours/src/main/kotlin/edu/artic/tours/TourDetailsFragment.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.bumptech.glide.Glide
 import com.fuzz.rx.bindToMain
+import com.fuzz.rx.defaultThrottle
 import com.fuzz.rx.disposedBy
 import com.jakewharton.rxbinding2.view.clicks
 import com.jakewharton.rxbinding2.widget.text
@@ -14,7 +15,9 @@ import edu.artic.adapter.itemChanges
 import edu.artic.analytics.ScreenCategoryName
 import edu.artic.base.utils.fromHtml
 import edu.artic.db.models.ArticTour
+import edu.artic.map.MapFragment
 import edu.artic.viewmodel.BaseViewModelFragment
+import edu.artic.viewmodel.Navigate
 import kotlinx.android.synthetic.main.cell_tour_details_stop.view.*
 import kotlinx.android.synthetic.main.fragment_tour_details.*
 import kotlin.reflect.KClass
@@ -96,6 +99,7 @@ class TourDetailsFragment : BaseViewModelFragment<TourDetailsViewModel>() {
                 .disposedBy(disposeBag)
 
         startTourButton.clicks()
+                .defaultThrottle()
                 .subscribe { viewModel.onClickStartTour() }
                 .disposedBy(disposeBag)
 
@@ -108,14 +112,27 @@ class TourDetailsFragment : BaseViewModelFragment<TourDetailsViewModel>() {
 
     override fun setupNavigationBindings(viewModel: TourDetailsViewModel) {
         viewModel.navigateTo
-                .subscribe {
-                    /**
-                     * navigate to map using args
-                     */
-//                    navController.navigate(
-//                            R.id.goToEventDetailsAction,
-//                            EventDetailFragment.argsBundle(endpoint.event)
-//                    )
+                .subscribe { navigationEndpoint ->
+                    when (navigationEndpoint) {
+                        is Navigate.Forward -> {
+                            when (navigationEndpoint.endpoint) {
+                                is TourDetailsViewModel.NavigationEndpoint.Map -> {
+                                    /**
+                                     * navigate to map using args
+                                     */
+                                    val endpoint = navigationEndpoint.endpoint as TourDetailsViewModel.NavigationEndpoint.Map
+                                    navController.navigate(
+                                            R.id.loadMap,
+                                            MapFragment.argsBundle(endpoint.tour)
+                                    )
+                                }
+
+                            }
+                        }
+                        is Navigate.Back -> {
+                        }
+                    }
+
                 }.disposedBy(disposeBag)
     }
 
