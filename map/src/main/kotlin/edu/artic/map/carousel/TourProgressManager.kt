c1ff2b4e73c58e7c600729d5d2e72286d0de697d
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-08-22 18:29:59 -0400

    [Map] - Adds map mode display switching logic

--- map/src/main/kotlin/edu/artic/map/carousel/TourProgressManager.kt
+++ map/src/main/kotlin/edu/artic/map/carousel/TourProgressManager.kt
@@ -1,7 +1,10 @@
 package edu.artic.map.carousel
 
+import com.fuzz.rx.Optional
+import edu.artic.db.models.ArticTour
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
+
 
 /**
  * Responsible for managing the communication between tour carousel and the map.
@@ -12,5 +15,8 @@ class TourProgressManager {
      * Selected ArticObject
      */
     val selectedStop: Subject<String> = BehaviorSubject.create()
-
+    /**
+     * Save the last selected tour.
+     */
+    val selectedTour: Subject<Optional<ArticTour>> = BehaviorSubject.createDefault(Optional(null))
 }
