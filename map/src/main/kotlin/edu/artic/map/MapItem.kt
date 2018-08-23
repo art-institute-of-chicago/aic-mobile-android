c61c88963310d9a3f32388f836913bb72fd666cf
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-23 18:15:35 -0400

    [Map] - Turn MapItem subclasses into 'data class'es

--- map/src/main/kotlin/edu/artic/map/MapItem.kt
+++ map/src/main/kotlin/edu/artic/map/MapItem.kt
@@ -15,28 +15,28 @@ import edu.artic.map.helpers.toLatLng
  *
  * @param floor the floor of the map where said marker should appear
  */
-sealed class MapItem<T>(val item: T, val floor: Int) {
+sealed class MapItem<T>(open val item: T, open val floor: Int) {
     /**
      * Rich-format label for an area of the map.
      */
-    class Annotation(item: ArticMapAnnotation, floor: Int) : MapItem<ArticMapAnnotation>(item, floor)
+    data class Annotation(override val item: ArticMapAnnotation, override val floor: Int) : MapItem<ArticMapAnnotation>(item, floor)
 
     /**
      * This acts as a sort of group for multiple [MapItem.Object]s.
      */
-    class Gallery(gallery: ArticGallery, floor: Int) : MapItem<ArticGallery>(gallery, floor)
+    data class Gallery(override val item: ArticGallery, override val floor: Int) : MapItem<ArticGallery>(item, floor)
 
     /**
      * This provides the greatest level of detail - it can represent individual art pieces.
      *
      * There are usually an order of magnitude more [Object]s than [Gallery]s.
      */
-    class Object(item: ArticObject, floor: Int) : MapItem<ArticObject>(item, floor)
+    data class Object(override val item: ArticObject, override val floor: Int) : MapItem<ArticObject>(item, floor)
 
     /**
      * This provides detail about the tour object.
      */
-    class TourIntro(item: ArticTour, floor: Int) : MapItem<ArticTour>(item, floor)
+    data class TourIntro(override val item: ArticTour, override val floor: Int) : MapItem<ArticTour>(item, floor)
 
     /**
      * TODO: This function is currently unused. Perhaps convert to abstract method?
