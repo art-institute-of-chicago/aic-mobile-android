3b8e6aeed0695840a1d48bface525f19c108aa77
Andrew Grosner <abgrosner@gmail.com>
2018-08-22 23:05:35 -0400

    [Audio Details] convert Language Adapter into common adapter interface. Create DropDownAdapter interface to give BaseRecyclerview adapter ability for dropdown.

--- adapter/src/main/java/edu/artic/adapter/BaseAdapterBinder.kt
+++ adapter/src/main/java/edu/artic/adapter/BaseAdapterBinder.kt
@@ -6,11 +6,48 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.BaseAdapter
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
 
 /**
  * Description:
  */
 fun <TModel> BaseRecyclerViewAdapter<TModel, *>.toBaseAdapter() = BaseAdapterBinder(this)
+
+/**
+ * Retrieve original [BaseRecyclerViewAdapter] from this [BaseAdapter]. It must be created with [toBaseAdapter] first.
+ * Must be a function due to Kotlin's type inference.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <TModel> SpinnerAdapter.baseRecyclerViewAdapter(): BaseRecyclerViewAdapter<TModel, BaseViewHolder> =
+        (this as BaseAdapterBinder<TModel>).adapter as BaseRecyclerViewAdapter<TModel, BaseViewHolder>
+
+/**
+ * Implement this interface to add the ability for dropdown view functionality within a [Spinner], for example.
+ */
+interface DropDownAdapter<TModel, VH : BaseViewHolder> {
+
+    /**
+     * Return the item view based on type. The headers + footers will not be called from here. Those
+     * cannot be configured to display differently in this adapter.
+     */
+    fun onCreateDropdownItemViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder? =
+            BaseViewHolder(parent, viewType).apply {
+                itemView.onDropdownHolderCreated(parent, viewType)
+            }
+
+    fun onBindDropdownItemViewHolder(baseViewHolder: VH, item: TModel, position: Int) =
+            baseViewHolder.itemView.onBindDropdownView(item, position)
+
+    fun View.onBindDropdownView(item: TModel, position: Int) = Unit
+
+    /**
+     * Called when the [BaseViewHolder] is first created in the scope of it's itemView. Perform
+     * initial registering of view bindings here that respond outside normal onClickListener.
+     */
+    @Suppress("UNUSED_PARAMETER", "unused")
+    fun View.onDropdownHolderCreated(parent: ViewGroup, viewType: Int) = Unit
+}
 
 /**
  * Description: Provides a very simple compatibility layer between [BaseRecyclerViewAdapter]
@@ -66,6 +103,35 @@ open class BaseAdapterBinder<TModel>(
 
         viewHolder?.let { adapter.bindViewHolder(viewHolder, i) }
         return localView
+    }
+
+    /**
+     * Defaults to the [getView] implementation if no helper is used.
+     */
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View? {
+        if (adapter is DropDownAdapter<*, *> && !adapter.isFooterPosition(position) && !adapter.isHeaderPosition(position)) {
+            @Suppress("UNCHECKED_CAST")
+            val dropDownAdapter = adapter as DropDownAdapter<TModel, BaseViewHolder>
+            var localView = convertView
+            val viewHolder: BaseViewHolder?
+            val viewType = adapter.getItemViewType(position)
+            if (localView == null || localView.getTag(R.id.tag_item_type) != viewType) {
+                viewHolder = adapter.onCreateDropdownItemViewHolder(parent, viewType)
+
+                if (viewHolder != null) {
+                    localView = viewHolder.itemView
+                    localView.setTag(R.id.tag_holder, viewHolder)
+                    localView.setTag(R.id.tag_item_type, viewType)
+                }
+            } else {
+                viewHolder = localView.getTag(R.id.tag_holder) as BaseViewHolder?
+            }
+
+            viewHolder?.let { dropDownAdapter.onBindDropdownItemViewHolder(viewHolder, adapter.getItem(position), position) }
+            return localView
+        } else {
+            return getView(position, convertView, parent)
+        }
     }
 
     private inner class RecyclerViewToBaseAdapterObserver : RecyclerView.AdapterDataObserver() {
