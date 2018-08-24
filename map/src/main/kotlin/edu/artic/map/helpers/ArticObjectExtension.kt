3233811fa2a05118b7cb9ce4d918d470c26a31f3
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-24 17:48:39 -0400

    [Map] - Don't crash if individual markers can't be modified at that instant
    It's not worth crashing over.

--- map/src/main/kotlin/edu/artic/map/helpers/ArticObjectExtension.kt
+++ map/src/main/kotlin/edu/artic/map/helpers/ArticObjectExtension.kt
@@ -1,8 +1,12 @@
 package edu.artic.map.helpers
 
+import android.support.annotation.UiThread
+import android.util.Log
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import edu.artic.db.models.ArticObject
 import edu.artic.db.models.ArticTour
+import edu.artic.map.BuildConfig
 
 fun ArticObject.toLatLng(): LatLng {
     val split = location?.split(",")
@@ -18,4 +22,27 @@ fun ArticTour.toLatLng(): LatLng {
             split?.first()?.toDouble() ?: 0.0,
             split?.last()?.toDouble() ?: 0.0
     )
+}
+
+/**
+ * Google's Map API throws exceptions if a marker is used when either
+ * 1. It is no longer attached to the map
+ * 2. It has an invalid icon
+ *
+ * Situation 1 is vastly more likely; this is the result of issues
+ * surrounding synchronization of access to the marker. This method
+ * will print a warning when it detects those scenarios instead of
+ * crashing the app.
+ *
+ * FIXME: Fix all such synchronization issues, then remove this method
+ */
+@UiThread
+fun <T> Marker.ifNotRemoved(action: (Marker) -> T) {
+    try {
+        action(this)
+    } catch (ex: IllegalArgumentException) {
+        if (BuildConfig.DEBUG) {
+            Log.w("MapMarker", ex.message)
+        }
+    }
 }
