c342f9f5aacd21205c97e4782f2730abbc162599
Andrew Grosner <abgrosner@gmail.com>
2018-08-23 21:03:48 -0400

    [Map] Extract conversion helper for amenity type to icon. Cleanup some of the verbose code in MapFragment.

---
+++ map/src/main/kotlin/edu/artic/map/util/MapUtils.kt
@@ -0,0 +1,31 @@
+package edu.artic.map.util
+
+import android.support.annotation.DrawableRes
+import edu.artic.db.models.ArticMapAmenityType
+import edu.artic.map.R
+import timber.log.Timber
+
+/**
+ * Description:
+ */
+@DrawableRes
+internal fun amenityIconForAmenityType(type: String?): Int {
+    return when (type) {
+        ArticMapAmenityType.WOMANS_ROOM -> R.drawable.icon_amenity_map_womens_room_blue
+        ArticMapAmenityType.MENS_ROOM -> R.drawable.icon_amenity_map_mens_room_blue
+        ArticMapAmenityType.ELEVATOR -> R.drawable.icon_amenity_map_elevator_blue
+        ArticMapAmenityType.GIFT_SHOP -> R.drawable.icon_amenity_map_shop_blue
+        ArticMapAmenityType.TICKETS -> R.drawable.icon_amenity_map_tickets_blue
+        ArticMapAmenityType.INFORMATION -> R.drawable.icon_amenity_map_information_blue
+        ArticMapAmenityType.CHECK_ROOM -> R.drawable.icon_amenity_map_check_room_blue
+        ArticMapAmenityType.AUDIO_GUIDE -> R.drawable.icon_amenity_map_audio_guide_blue
+        ArticMapAmenityType.WHEELCHAIR_RAMP -> R.drawable.icon_amenity_map_wheelchair_ramp_blue
+        ArticMapAmenityType.DINING -> R.drawable.icon_amenity_map_restaurant_blue
+        ArticMapAmenityType.FAMILY_RESTROOM -> R.drawable.icon_amenity_map_family_restroom_blue
+        ArticMapAmenityType.MEMBERS_LOUNGE -> R.drawable.icon_amenity_map_cafe_blue
+        else -> {
+            Timber.d("unknownAmenityType: $type")
+            0
+        }
+    }
+}
