8610fe49234056521d58caa0fb9bbb8832a987b7
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-29 18:22:31 -0400

    [Audio] - Add ViewModel for the activity to use for that deep-link-like logic

--- audio/src/main/java/edu/artic/audio/AudioModule.kt
+++ audio/src/main/java/edu/artic/audio/AudioModule.kt
@@ -25,6 +25,11 @@ abstract class AudioModule {
     @ViewModelKey(AudioLookupViewModel::class)
     abstract fun lookupViewModel(audioLookupViewModel: AudioLookupViewModel): ViewModel
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(AudioNavViewModel::class)
+    abstract fun audioNavViewModel(audioNavViewModel: AudioNavViewModel): ViewModel
+
     @get:ContributesAndroidInjector
     abstract val audioLookupFragment: AudioLookupFragment
 
---
+++ audio/src/main/java/edu/artic/audio/AudioNavViewModel.kt
@@ -0,0 +1,12 @@
+package edu.artic.audio
+
+import edu.artic.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+
+/**
+ * Representation of excess [AudioActivity] logic.
+ */
+class AudioNavViewModel @Inject constructor() : BaseViewModel() {
+
+}
