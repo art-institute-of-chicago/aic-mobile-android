60ad1e4d287e8f01089a2395bf4664717917a3b5
fuzzagrosner <andrew@fuzzproductions.com>
2018-08-29 09:46:40 -0400

    [AppData] Allow injection into AppDataManager.

--- db/src/main/kotlin/edu/artic/db/ApiModule.kt
+++ db/src/main/kotlin/edu/artic/db/ApiModule.kt
@@ -5,11 +5,20 @@ import android.os.Handler
 import android.os.Looper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.Multibinds
+import edu.artic.db.daos.ArticAudioFileDao
 import edu.artic.db.daos.ArticDataObjectDao
+import edu.artic.db.daos.ArticEventDao
+import edu.artic.db.daos.ArticExhibitionCMSDao
+import edu.artic.db.daos.ArticExhibitionDao
+import edu.artic.db.daos.ArticGalleryDao
+import edu.artic.db.daos.ArticMapAnnotationDao
+import edu.artic.db.daos.ArticObjectDao
+import edu.artic.db.daos.ArticTourDao
+import edu.artic.db.daos.DashboardDao
+import edu.artic.db.daos.GeneralInfoDao
 import edu.artic.db.progress.DownloadProgressInterceptor
 import edu.artic.db.progress.ProgressEventBus
 import okhttp3.OkHttpClient
@@ -27,13 +36,45 @@ abstract class ApiModule {
     @get:AdapterFactory
     abstract val jsonAdapterFactorySet: MutableSet<JsonAdapter.Factory>
 
-    @Binds
-    abstract fun appDataManager(appDataManager: AppDataManager): AppDataManager
-
     @Module
     companion object {
 
         private const val DEFAULT_TIMEOUT = 10L
+
+        @JvmStatic
+        @Provides
+        @Singleton
+        fun provideAppDataManager(
+                serviceProvider: AppDataServiceProvider,
+                appDataPreferencesManager: AppDataPreferencesManager,
+                appDatabase: AppDatabase,
+                dashboardDao: DashboardDao,
+                generalInfoDao: GeneralInfoDao,
+                audioFileDao: ArticAudioFileDao,
+                galleryDao: ArticGalleryDao,
+                tourDao: ArticTourDao,
+                exhibitionCMSDao: ArticExhibitionCMSDao,
+                mapAnnotationDao: ArticMapAnnotationDao,
+                dataObjectDao: ArticDataObjectDao,
+                eventDao: ArticEventDao,
+                exhibitionDao: ArticExhibitionDao,
+                objectDao: ArticObjectDao
+        ): AppDataManager = AppDataManager(
+                serviceProvider,
+                appDataPreferencesManager,
+                appDatabase,
+                dashboardDao,
+                generalInfoDao,
+                audioFileDao,
+                galleryDao,
+                tourDao,
+                exhibitionCMSDao,
+                mapAnnotationDao,
+                dataObjectDao,
+                eventDao,
+                exhibitionDao,
+                objectDao
+        )
 
         @JvmStatic
         @Provides
