7c32a099f10d9cbc9727fa08799f6f7501f20b6d
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-29 18:23:46 -0400

    [Refactor] - Move said logic from AudioActivity into ANVM
    This drops some of the pollution that had accumulated in the Activity.

--- audio/src/main/java/edu/artic/audio/AudioActivity.kt
+++ audio/src/main/java/edu/artic/audio/AudioActivity.kt
@@ -1,41 +1,35 @@
 package edu.artic.audio
 
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 import edu.artic.base.utils.disableShiftMode
 import edu.artic.media.ui.NarrowAudioPlayerFragment
-import edu.artic.media.ui.NarrowAudioPlayerFragment.Companion.ARG_SKIP_TO_DETAILS
 import edu.artic.navigation.NavigationSelectListener
-import edu.artic.ui.BaseActivity
+import edu.artic.viewmodel.BaseViewModelActivity
 import kotlinx.android.synthetic.main.activity_audio.*
+import kotlin.reflect.KClass
 
 /**
  * One of the four primary sections of the app. This Activity may host either
  * an [AudioLookupFragment] or an [AudioDetailsFragment].
  *
  * Note that a [NarrowAudioPlayerFragment] anywhere in the app may deep-link
- * to this screen. When it does so, we record that state in [willNavigate]
- * and perform the navigation in [onStart]. This prevents us from accidentally
- * showing [AudioLookupFragment] first.
+ * to this screen. When it does so, we record that state in
+ * [AudioNavViewModel.willNavigate] and perform the navigation in [onStart].
+ * This prevents us from accidentally showing [AudioLookupFragment] first.
  */
-class AudioActivity : BaseActivity() {
+class AudioActivity : BaseViewModelActivity<AudioNavViewModel>() {
+
+    override val viewModelClass: KClass<AudioNavViewModel>
+        get() = AudioNavViewModel::class
 
     override val layoutResId: Int
         get() = R.layout.activity_audio
 
-    /**
-     * Lifecycle-check set by [onCreate] and consumed by [onStart].
-     *
-     * Always reset to false by [onDestroy].
-     */
-    private var willNavigate = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val extras: Bundle = intent.extras ?: Bundle.EMPTY
-
-        willNavigate = extras.getBoolean(ARG_SKIP_TO_DETAILS, false)
+        viewModel.detectDetailsScreenLink(intent)
 
         bottomNavigation.apply {
             disableShiftMode(R.color.audio_menu_color_list)
@@ -47,17 +41,11 @@ class AudioActivity : BaseActivity() {
     override fun onStart() {
         super.onStart()
 
-        if (willNavigate) {
-            willNavigate = false
-            val navFragment = supportFragmentManager.primaryNavigationFragment
-            if (navFragment is NavHostFragment) {
-                navFragment.navController.navigate(R.id.see_current_audio_details)
-            }
-        }
+        viewModel.navigateToAudioDetailsScreen(supportFragmentManager)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        willNavigate = false
+        viewModel.clearNavigationState()
     }
 }
