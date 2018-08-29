de35caec939938e4bea896b3464f5b9e1f523c40
fuzzagrosner <andrew@fuzzproductions.com>
2018-08-29 09:36:31 -0400

    [Map] linking map to zoomlevel on the map.

--- map/src/main/kotlin/edu/artic/map/MapConstructs.kt
+++ map/src/main/kotlin/edu/artic/map/MapConstructs.kt
@@ -1,5 +1,14 @@
 package edu.artic.map
 
+import edu.artic.db.models.ArticTour
+
+data class MapChangeEvent(val focus: MapFocus, val floor: Int, val displayMode: MapDisplayMode)
+
+const val ZOOM_LANDMARK: ZoomLevel = 18.0f
+const val ZOOM_DEPARTMENTS: ZoomLevel = 19.0f
+const val ZOOM_DEPARTMENT_AND_SPACES: ZoomLevel = 21.0f
+const val ZOOM_INDIVIDUAL: ZoomLevel = 22.0f
+
 /**
  * Description: Defines at a particular map zoom level, what kind of [MapItem] we are to display
  */
@@ -7,5 +16,30 @@ enum class MapFocus {
     Landmark,
     Department,
     DepartmentAndSpaces,
-    Individual
+    Individual;
+
+    fun toZoomLevel(): ZoomLevel = when (this) {
+        Landmark -> ZOOM_LANDMARK
+        Department -> ZOOM_DEPARTMENTS
+        DepartmentAndSpaces -> ZOOM_DEPARTMENT_AND_SPACES
+        Individual -> ZOOM_INDIVIDUAL
+    }
+}
+
+typealias ZoomLevel = Float
+
+/**
+ * Convers a [ZoomLevel] int into a
+ */
+internal fun ZoomLevel.toMapFocus(): MapFocus = when {
+    this < ZOOM_LANDMARK -> MapFocus.Landmark
+    this < ZOOM_DEPARTMENTS -> MapFocus.Department
+    this < ZOOM_DEPARTMENT_AND_SPACES -> MapFocus.DepartmentAndSpaces
+    else -> MapFocus.Individual
+}
+
+sealed class MapDisplayMode {
+    data class Tour(val tour: ArticTour) : MapDisplayMode()
+    object CurrentFloor : MapDisplayMode()
+    data class Search<T>(val item: T) : MapDisplayMode()
 }
