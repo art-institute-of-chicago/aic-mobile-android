8efba6974ae19f0501fd9a04167514b0323f4609
fuzzagrosner <andrew@fuzzproductions.com>
2018-08-29 09:31:36 -0400

    [Map] start implementing render marker logic.

--- map/src/main/kotlin/edu/artic/map/ExploreMapMarkerConstructor.kt
+++ map/src/main/kotlin/edu/artic/map/ExploreMapMarkerConstructor.kt
@@ -1,13 +1,11 @@
 package edu.artic.map
 
+import android.content.Context
 import com.fuzz.rx.DisposeBag
-import com.fuzz.rx.asFlowable
-import com.fuzz.rx.bindTo
 import com.google.android.gms.maps.GoogleMap
 import edu.artic.db.daos.ArticGalleryDao
 import edu.artic.db.daos.ArticMapAnnotationDao
 import edu.artic.db.daos.ArticObjectDao
-import edu.artic.db.models.ArticMapAnnotation
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.Observables
@@ -20,7 +18,8 @@ import javax.inject.Inject
 class ExploreMapMarkerConstructor
 @Inject constructor(private val articMapAnnotationDao: ArticMapAnnotationDao,
                     private val galleryDao: ArticGalleryDao,
-                    private val objectsDao: ArticObjectDao) {
+                    private val objectsDao: ArticObjectDao,
+                    private val appContext: Context) { // App context, won't leak.
 
 
     lateinit var map: GoogleMap
@@ -34,6 +33,7 @@ class ExploreMapMarkerConstructor
     private val galleriesMapItemRenderer = GalleriesMapItemRenderer(galleryDao)
     private val objectsMapItemRenderer = ObjectsMapItemRenderer(objectsDao)
 
+    private val textMarkerGenerator: TextMarkerGenerator = TextMarkerGenerator(appContext)
 
     /**
      * Constructs handling for the floor and focus changes on the map.
@@ -43,23 +43,8 @@ class ExploreMapMarkerConstructor
         // buffer changes between the two events.
         val bufferedFloorFocus = Observables.combineLatest(focusChanges, floorChanges)
                 .toFlowable(BackpressureStrategy.LATEST)
+                .share()
 
-        bufferedFloorFocus
-                .flatMap { (focus, floor) ->
-                    // no longer renderable, we
-                    if (!landmarkMapItemRenderer.visibleMapFocus.contains(focus)) {
-                        emptyList<ArticMapAnnotation>().asFlowable()
-                    } else {
-                        landmarkMapItemRenderer.getAnnotationsAtFloor(floor)
-                    }
-                }
-                .map { items ->
-                    items.map {
-
-                    }
-                }
-                .bindTo()
-                .disposedBy(disposeBag)
 
     }
 }
