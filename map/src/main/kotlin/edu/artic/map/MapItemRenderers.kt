fe2ff9099324aab1270be1b4324eb9894a40b400
fuzzagrosner <andrew@fuzzproductions.com>
2018-08-29 10:11:49 -0400

    [Markers] Add image queue for prefetching bitmaps.

--- map/src/main/kotlin/edu/artic/map/MapItemRenderers.kt
+++ map/src/main/kotlin/edu/artic/map/MapItemRenderers.kt
@@ -6,7 +6,7 @@ import com.bumptech.glide.request.RequestOptions
 import com.fuzz.rx.DisposeBag
 import com.fuzz.rx.Optional
 import com.fuzz.rx.asFlowable
-import com.fuzz.rx.asObservable
+import com.fuzz.rx.bindTo
 import com.fuzz.rx.disposedBy
 import com.fuzz.rx.optionalOf
 import com.google.android.gms.maps.GoogleMap
@@ -35,23 +35,66 @@ import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.withLatestFrom
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 data class MarkerHolder<T>(val id: String,
                            val item: T,
                            val marker: Marker)
 
 data class MapItemRendererEvent<T>(val map: GoogleMap, val mapChangeEvent: MapChangeEvent, val items: List<T>)
+data class DelayedMapItemRenderEvent<T>(val originalEvent: MapItemRendererEvent<T>,
+                                        val item: T,
+                                        val bitmap: BitmapDescriptor)
 
 abstract class MapItemRenderer<T> {
 
     private val mapItems: Subject<Map<String, MarkerHolder<T>>> = BehaviorSubject.createDefault(emptyMap())
     private var currentFloor: Int = Int.MIN_VALUE
-    private val disposeBag: DisposeBag = DisposeBag()
+
+    private val imageQueueDisposeBag = DisposeBag()
+    private val imageFetcherDisposeBag = DisposeBag()
+    private val bitmapQueue: Subject<DelayedMapItemRenderEvent<T>> = PublishSubject.create()
 
     // this should be the inflated view's context
     lateinit var context: Context
+
+    init {
+        bitmapQueue
+                .buffer(500, TimeUnit.MILLISECONDS, 30)
+                .filter { it.isNotEmpty() }
+                .withLatestFrom(mapItems)
+                .observeOn(AndroidSchedulers.mainThread())
+                .map { (newMarkers, existingMarkers) ->
+                    Timber.d("Updating with ${newMarkers.size} to ${existingMarkers.size}")
+                    newMarkers
+                            .asSequence()
+                            .mapNotNull { item ->
+                                val id = getIdFromItem(item.item)
+                                // only construct things that actually exist in the current mapItems list.
+                                // slight chance that the items here will still exist when attemping map
+                                // propagation.
+                                val existing = existingMarkers[id]
+                                if (existing != null) {
+                                    // remove existing marker when found, we're overloading the existing one.
+                                    existing.marker.remove()
+
+                                    val (map, _, _) = item.originalEvent
+                                    val (_, floor, displayMode) = item.originalEvent.mapChangeEvent
+                                    constructAndAddMarkerHolder(item = item.item,
+                                            bitmap = item.bitmap,
+                                            displayMode = displayMode,
+                                            map = map,
+                                            floor = floor,
+                                            id = getIdFromItem(item.item))
+                                } else null
+                            }.associateBy { it.id }
+                }
+                .bindTo(mapItems)
+                .disposedBy(imageQueueDisposeBag)
+    }
 
     /**
      * Return what map focus level these [MapItem] display at.
@@ -70,7 +113,17 @@ abstract class MapItemRenderer<T> {
 
     abstract fun getIdFromItem(item: T): String
 
-    abstract fun getBitmap(item: T, displayMode: MapDisplayMode): Observable<BitmapDescriptor>
+    /**
+     * Fast implementation that comes from resources, or an already available asset. If null, we delay the
+     * load of the marker until [getBitmapFetcher] completes (if available).
+     */
+    open fun getFastBitmap(item: T, displayMode: MapDisplayMode): BitmapDescriptor? = null
+
+    /**
+     * If implemented, we enqueue a new [Observable] that when completes, forwards its data
+     * and re-renders an existing [MarkerHolder]
+     */
+    open fun getBitmapFetcher(item: T, displayMode: MapDisplayMode): Observable<BitmapDescriptor>? = null
 
     open fun MarkerOptions.configureMarkerOptions(floor: Int, mapDisplayMode: MapDisplayMode, item: T) = Unit
 
@@ -87,10 +140,16 @@ abstract class MapItemRenderer<T> {
         this.mapItems.onNext(markers.associateBy { it.id })
     }
 
-    private fun renderMarkers(map: Observable<GoogleMap>, floorFocus: Flowable<MapChangeEvent>)
+    @Synchronized
+    private fun enqueueBitmapFetch(fetcher: Observable<DelayedMapItemRenderEvent<T>>) {
+        fetcher.subscribeBy { bitmapQueue.onNext(it) }
+                .disposedBy(imageFetcherDisposeBag)
+    }
+
+    private fun renderMarkers(mapObservable: Observable<GoogleMap>, floorFocus: Flowable<MapChangeEvent>)
             : Flowable<List<MarkerHolder<T>>> {
         return floorFocus
-                .withLatestFrom(map.toFlowable(BackpressureStrategy.LATEST)) { first, second -> first to second }
+                .withLatestFrom(mapObservable.toFlowable(BackpressureStrategy.LATEST)) { first, second -> first to second }
                 .debug("New Map Event")
                 .observeOn(Schedulers.io())
                 .flatMap { (mapEvent, map) ->
@@ -109,52 +168,98 @@ abstract class MapItemRenderer<T> {
                 .observeOn(AndroidSchedulers.mainThread())
                 .withLatestFrom(mapItems.toFlowable(BackpressureStrategy.LATEST)) { event, mapItems -> event to mapItems }
                 .flatMap { (event, existingMapItems) ->
-                    // separate the list into two separate lists, one for items to be removed
-                    // one for existing items in the new list too.
-                    val (existingFoundItems, toBeRemoved) = existingMapItems.values.partition { event.items.contains(it.item) }
-
-                    // trim down items not in the new list
-                    toBeRemoved.forEach { it.marker.remove() }
-
-                    // re-associate the existing found items that are still in the list when a change happens.
-                    val existingFoundItemsMap = existingFoundItems.associateBy { it.id }
+                    val (map, _, items) = event
+                    val (_, floor, displayMode) = event.mapChangeEvent
+                    // TODO: for now cancel all running image requests.
+                    imageFetcherDisposeBag.clear()
+                    val foundItemsMap = removeOldMarkers(existingMapItems, items)
 
                     // don't emit an empty event (which empty zip list does), rather emit an
                     // empty list here.
-                    if (event.items.isEmpty()) {
-                        return@flatMap listOf<MarkerHolder<T>>().asFlowable()
-                    }
-                    Flowable.zip(event.items.map { item ->
-                        val id = getIdFromItem(item)
-                        val existing = existingFoundItemsMap[id]
-                        // same item, don't re-add to the map.
-                        existing?.asFlowable()
-                                ?: getBitmap(item, event.mapChangeEvent.displayMode)
-                                        .map { bitmap ->
-                                            val options = MarkerOptions()
-                                                    .zIndex(zIndex)
-                                                    .position(getLocationFromItem(item))
-                                                    .icon(bitmap)
-                                                    .apply {
-                                                        configureMarkerOptions(event.mapChangeEvent.floor,
-                                                                event.mapChangeEvent.displayMode,
-                                                                item)
-                                                    }
-                                            MarkerHolder(
-                                                    id,
-                                                    item,
-                                                    event.map.addMarker(options))
+                    if (items.isEmpty()) {
+                        listOf<MarkerHolder<T>>().asFlowable()
+                    } else {
+                        Flowable.zip(items.mapNotNull { item ->
+                            val id = getIdFromItem(item)
+                            val existing = foundItemsMap[id]
+                            // same item, don't re-add to the map.
+                            if (existing != null) {
+                                existing.asFlowable()
+                            } else {
+                                getBitmapFetcher(item, displayMode)?.let { bitmapFetcher ->
+                                    enqueueBitmapFetch(bitmapFetcher.map { DelayedMapItemRenderEvent(event, item, it) })
+                                }
+                                Flowable.fromCallable { Optional(getFastBitmap(item, displayMode)) }
+                                        .subscribeOn(Schedulers.computation())
+                                        .observeOn(AndroidSchedulers.mainThread())
+                                        .map { optionalBitmap ->
+                                            optionalOf(optionalBitmap.value?.let { bitmap ->
+                                                constructAndAddMarkerHolder(
+                                                        item = item,
+                                                        bitmap = bitmap,
+                                                        displayMode = displayMode,
+                                                        map = map,
+                                                        floor = floor,
+                                                        id = id)
+                                            })
                                         }
-                                        .toFlowable(BackpressureStrategy.LATEST)
-
-                    }) { markers ->
-                        // RX Zip does not return a properly typed array, it is an Object[] from this zip.
-                        @Suppress("UNCHECKED_CAST")
-                        (markers as Array<Any>)
-                                .mapTo(mutableListOf()) { item -> item as MarkerHolder<T> }
-                                .toList()
+                            }
+                        }) { markers ->
+                            // RX Zip does not return a properly typed array, it is an Object[] from this zip.
+                            @Suppress("UNCHECKED_CAST")
+                            (markers as Array<Any>)
+                                    .mapNotNullTo(mutableListOf()) { item ->
+                                        // ugly cast necessary.
+                                        when (item) {
+                                            is MarkerHolder<*> -> item as MarkerHolder<T>?
+                                            is Optional<*> -> item.value as MarkerHolder<T>?
+                                            else -> null
+                                        }
+                                    }
+                                    .toList()
+                        }
                     }
                 }
+    }
+
+    fun removeOldMarkers(existingMapItems: Map<String, MarkerHolder<T>>, items: List<T>): Map<String, MarkerHolder<T>> {
+        // separate the list into two separate lists, one for items to be removed
+        // one for existing items in the new list too.
+        val (existingFoundItems, toBeRemoved) = existingMapItems.values.partition { items.contains(it.item) }
+
+        // trim down items not in the new list
+        toBeRemoved.forEach { it.marker.remove() }
+
+        // re-associate the existing found items that are still in the list when a change happens.
+        val existingFoundItemsMap = existingFoundItems.associateBy { it.id }
+        return existingFoundItemsMap
+    }
+
+    /**
+     * Constructs the [MarkerHolder] based on a few parameters. Also adds the [MarkerHolder] to
+     * the [GoogleMap].
+     */
+    private fun constructAndAddMarkerHolder(
+            item: T,
+            bitmap: BitmapDescriptor,
+            displayMode: MapDisplayMode,
+            map: GoogleMap,
+            floor: Int,
+            id: String
+    ): MarkerHolder<T> {
+        val options = MarkerOptions()
+                .zIndex(zIndex)
+                .position(getLocationFromItem(item))
+                .icon(bitmap)
+                .apply {
+                    configureMarkerOptions(floor,
+                            displayMode,
+                            item)
+                }
+        return MarkerHolder(
+                id,
+                item,
+                map.addMarker(options))
     }
 
 }
@@ -181,8 +286,8 @@ class LandmarkMapItemRenderer(articMapAnnotationDao: ArticMapAnnotationDao) : Ma
 
     override fun getVisibleMapFocus(displayMode: MapDisplayMode): Set<MapFocus> = setOf(MapFocus.Landmark)
 
-    override fun getBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): Observable<BitmapDescriptor> =
-            BitmapDescriptorFactory.fromBitmap(textMarkerGenerator.makeIcon(item.label.orEmpty())).asObservable()
+    override fun getFastBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): BitmapDescriptor =
+            BitmapDescriptorFactory.fromBitmap(textMarkerGenerator.makeIcon(item.label.orEmpty()))
 
     override val zIndex: Float = 1.0f
 }
@@ -198,8 +303,8 @@ class SpacesMapItemRenderer(articMapAnnotationDao: ArticMapAnnotationDao)
 
     override fun getVisibleMapFocus(displayMode: MapDisplayMode): Set<MapFocus> = setOf(MapFocus.DepartmentAndSpaces, MapFocus.Individual)
 
-    override fun getBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): Observable<BitmapDescriptor> =
-            BitmapDescriptorFactory.fromBitmap(textMarkerGenerator.makeIcon(item.label.orEmpty())).asObservable()
+    override fun getFastBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): BitmapDescriptor =
+            BitmapDescriptorFactory.fromBitmap(textMarkerGenerator.makeIcon(item.label.orEmpty()))
 
     override val zIndex: Float = 1.0f
 }
@@ -211,8 +316,8 @@ class AmenitiesMapItemRenderer(articMapAnnotationDao: ArticMapAnnotationDao) : M
 
     override fun getVisibleMapFocus(displayMode: MapDisplayMode): Set<MapFocus> = MapFocus.values().toSet() // all zoom levels
 
-    override fun getBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): Observable<BitmapDescriptor> {
-        return BitmapDescriptorFactory.fromResource(amenityIconForAmenityType(item.amenityType)).asObservable()
+    override fun getFastBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): BitmapDescriptor {
+        return BitmapDescriptorFactory.fromResource(amenityIconForAmenityType(item.amenityType))
     }
 
     override val zIndex: Float = 0.0f
@@ -229,7 +334,7 @@ class DepartmentsMapItemRenderer(articMapAnnotationDao: ArticMapAnnotationDao)
 
     override fun getVisibleMapFocus(displayMode: MapDisplayMode): Set<MapFocus> = setOf(MapFocus.Department, MapFocus.DepartmentAndSpaces)
 
-    override fun getBitmap(item: ArticMapAnnotation, displayMode: MapDisplayMode): Observable<BitmapDescriptor> {
+    override fun getBitmapFetcher(item: ArticMapAnnotation, displayMode: MapDisplayMode): Observable<BitmapDescriptor>? {
         return Glide.with(context)
                 .asBitmap()
                 .load(item.imageUrl?.asCDNUri())
@@ -253,8 +358,8 @@ class GalleriesMapItemRenderer(private val galleriesDao: ArticGalleryDao)
 
     override fun getLocationFromItem(item: ArticGallery): LatLng = item.toLatLng()
 
-    override fun getBitmap(item: ArticGallery, displayMode: MapDisplayMode): Observable<BitmapDescriptor> =
-            BitmapDescriptorFactory.fromBitmap(textMarkerGenerator.makeIcon(item.displayTitle)).asObservable()
+    override fun getFastBitmap(item: ArticGallery, displayMode: MapDisplayMode): BitmapDescriptor =
+            BitmapDescriptorFactory.fromBitmap(textMarkerGenerator.makeIcon(item.displayTitle))
 
     override fun getIdFromItem(item: ArticGallery): String = item.galleryId.orEmpty()
 
@@ -282,7 +387,7 @@ class ObjectsMapItemRenderer(private val objectsDao: ArticObjectDao)
 
     override fun getIdFromItem(item: ArticObject): String = item.id.toString()
 
-    override fun getBitmap(item: ArticObject, displayMode: MapDisplayMode): Observable<BitmapDescriptor> =
+    override fun getBitmapFetcher(item: ArticObject, displayMode: MapDisplayMode): Observable<BitmapDescriptor>? =
             Glide.with(context)
                     .asBitmap()
                     .apply(RequestOptions().disallowHardwareConfig())
@@ -294,19 +399,7 @@ class ObjectsMapItemRenderer(private val objectsDao: ArticObjectDao)
                     .asRequestObservable(context)
                     .debug("Glide loading: ${item.title}")
                     .map { bitmap ->
-                        var order: String? = null
-                        if (displayMode is MapDisplayMode.Tour) {
-                            /**
-                             * If map's display mode is Tour, get the order number of the stop.
-                             */
-                            val index = displayMode.tour
-                                    .tourStops
-                                    .indexOfFirst { it.objectId == item.nid }
-
-                            if (index > -1) {
-                                order = (index + 1).toString()
-                            }
-                        }
+                        val order: String? = item.getTourOrderNumberBasedOnDisplayMode(displayMode)
                         BitmapDescriptorFactory.fromBitmap(
                                 articObjectMarkerGenerator.makeIcon(bitmap, overlay = order))
                     }
