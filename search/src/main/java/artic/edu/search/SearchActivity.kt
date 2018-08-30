784cdf81fc3cbecac46dbcb18eef0b4a313b7bf0
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-08-30 13:53:03 -0400

    [Search] - Navigation : Provides bottom nagivation to search

--- search/src/main/java/artic/edu/search/SearchActivity.kt
+++ search/src/main/java/artic/edu/search/SearchActivity.kt
@@ -1,6 +1,11 @@
 package artic.edu.search
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import edu.artic.base.utils.disableShiftMode
+import edu.artic.navigation.NavigationSelectListener
 import edu.artic.viewmodel.BaseViewModelActivity
+import kotlinx.android.synthetic.main.activity_search.*
 import kotlin.reflect.KClass
 
 class SearchActivity : BaseViewModelActivity<SearchViewModel>() {
@@ -10,4 +15,9 @@ class SearchActivity : BaseViewModelActivity<SearchViewModel>() {
     override val layoutResId: Int
         get() = R.layout.activity_search
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bottomNavigation.disableShiftMode(R.color.menu_color_list)
+        bottomNavigation.setOnNavigationItemSelectedListener(NavigationSelectListener(this))
+    }
 }
