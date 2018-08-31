1d89036e56e93a0d38d4e80b583019710984f6e5
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-08-31 14:51:24 -0400

    [SearchAnalytics] - Sends 'text abandoned' event

--- search/src/main/java/artic/edu/search/SearchViewModel.kt
+++ search/src/main/java/artic/edu/search/SearchViewModel.kt
@@ -1,9 +1,16 @@
 package artic.edu.search
 
+import edu.artic.analytics.AnalyticsAction
+import edu.artic.analytics.AnalyticsTracker
+import edu.artic.analytics.ScreenCategoryName
 import edu.artic.viewmodel.BaseViewModel
 import javax.inject.Inject
 
-class SearchViewModel @Inject constructor() : BaseViewModel() {
+class SearchViewModel @Inject constructor(private val analyticsTracker: AnalyticsTracker) : BaseViewModel() {
+
+    fun clearText() {
+        analyticsTracker.reportEvent(ScreenCategoryName.Search, AnalyticsAction.searchAbandoned)
+    }
 
 }
 
