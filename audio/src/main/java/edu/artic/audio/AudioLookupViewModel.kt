671882f2e5208fc16f7a080f63a6264634e20bae
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-08-31 11:08:53 -0400

    [AudioLookup] - Add mechanism for the actual 'lookup' task
    This covers looking up an audio file by its primary selector number.
    Future work would be needed to cover non-primary selector numbers.

--- audio/src/main/java/edu/artic/audio/AudioLookupViewModel.kt
+++ audio/src/main/java/edu/artic/audio/AudioLookupViewModel.kt
@@ -1,7 +1,12 @@
 package edu.artic.audio
 
+import com.fuzz.rx.disposedBy
 import edu.artic.audio.NumberPadElement.*
+import edu.artic.db.daos.ArticObjectDao
+import edu.artic.db.models.ArticObject
 import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
@@ -12,10 +17,24 @@ import javax.inject.Inject
  *
  * For the ViewModel shown for details about a single audio file, check out
  * [AudioDetailsViewModel] instead.
+ *
+ * For lookup, we always query [ArticObject.objectSelectorNumber].
  */
-class AudioLookupViewModel @Inject constructor() : BaseViewModel() {
+class AudioLookupViewModel @Inject constructor(objectLookupDao: ArticObjectDao) : BaseViewModel() {
 
     val adapterClicks: Subject<NumberPadElement> = PublishSubject.create()
+
+    /**
+     * New requests for audio lookup. Each entry to this will trigger a response,
+     * published on [lookupResults].
+     */
+    val lookupRequests: Subject<String> = PublishSubject.create()
+
+    /**
+     * Responses to search queries [passed in][io.reactivex.Observer.onNext] to
+     * [lookupRequests].
+     */
+    val lookupResults: Subject<LookupResult> = PublishSubject.create()
 
     /**
      * See [NumberPadAdapter] for details on all this.
@@ -36,4 +55,52 @@ class AudioLookupViewModel @Inject constructor() : BaseViewModel() {
             GoSearch
     ))
 
+    init {
+        // This connects 'lookupRequests' to 'lookupResults' via an IO query.
+        lookupRequests
+                .observeOn(Schedulers.io())
+                .subscribeBy {
+                    val foundObject = objectLookupDao.getObjectBySelectorNumber(it)
+
+                    val result = if (foundObject == null) {
+                        LookupResult.NotFound(it)
+                    } else {
+                        LookupResult.FoundAudio(foundObject)
+                    }
+
+                    lookupResults.onNext(result)
+
+                }.disposedBy(disposeBag)
+    }
+
+}
+
+/**
+ * Constant class for the asynchronous response to looking for an
+ * [ArticObject] by [selector id][ArticObject.objectSelectorNumber].
+ *
+ * Two known subclasses:
+ * * [FoundAudio]
+ * * [NotFound]
+ */
+sealed class LookupResult {
+
+    /**
+     * Audio was found! You can pass [hostObject] to the
+     * [AudioPlayerService][edu.artic.media.audio.AudioPlayerService]
+     * if you want to play it.
+     *
+     * NB: [hostObject] can be safely assumed to contain at least one
+     * [ArticAudioFile][edu.artic.db.models.ArticAudioFile].
+     *
+     * @see AudioLookupViewModel.lookupRequests
+     */
+    class FoundAudio(val hostObject: ArticObject) : LookupResult()
+
+    /**
+     * The requested id was found.
+     *
+     * TODO: Add analytics event indicating that this query was not found.
+     */
+    class NotFound(val searchQuery: String) : LookupResult()
 }
