d119205977e8729530220972254de5208ad5bc75
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-04 14:05:26 -0400

    [Refactor] - Translation Spinner : Makes LanguageAdapter generic

--- audio/src/main/java/edu/artic/audio/LanguageAdapter.kt
+++ audio/src/main/java/edu/artic/audio/LanguageAdapter.kt
@@ -6,6 +6,7 @@ import edu.artic.adapter.AutoHolderRecyclerViewAdapter
 import edu.artic.adapter.BaseViewHolder
 import edu.artic.adapter.DropDownAdapter
 import edu.artic.db.models.AudioFileModel
+import edu.artic.localization.SpecifiesLanguage
 import kotlinx.android.synthetic.main.view_language_box.view.*
 
 /**
@@ -15,7 +16,7 @@ import kotlinx.android.synthetic.main.view_language_box.view.*
  * of the list (i.e. the 'currently selected' language).
  */
 class LanguageAdapter : AutoHolderRecyclerViewAdapter<AudioFileModel>(),
-        DropDownAdapter<AudioFileModel, BaseViewHolder> {
+        DropDownAdapter<SpecifiesLanguage, BaseViewHolder> {
     override fun View.onBindView(item: AudioFileModel, position: Int) {
         text.text = item.userFriendlyLanguage(context)
         text.setTextColor(Color.WHITE)
@@ -23,7 +24,7 @@ class LanguageAdapter : AutoHolderRecyclerViewAdapter<AudioFileModel>(),
 
     override fun getLayoutResId(position: Int): Int = R.layout.view_language_box
 
-    override fun View.onBindDropdownView(item: AudioFileModel, position: Int) {
+    override fun View.onBindDropdownView(item: SpecifiesLanguage, position: Int) {
         text.text = item.userFriendlyLanguage(context)
     }
 }
