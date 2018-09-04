be41b8fc441903b42d330f7b6b85a6e8afe328e9
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-04 11:08:38 -0400

    [Refactor] - Documentation : Adds documentation for Constants file

--- db/src/main/kotlin/edu/artic/db/Constants.kt
+++ db/src/main/kotlin/edu/artic/db/Constants.kt
@@ -1,7 +1,12 @@
 package edu.artic.db
 
 /**
+ * This file contains the constant values used in the models.
  * @author Sameer Dhakal (Fuzz)
  */
 
+/**
+ * [INVALID_FLOOR] represents the invalid floor number.
+ * Since negative numbers are valid number for underground floors we are using Int.MIN_VALUE.
+ */
 const val INVALID_FLOOR: Int = Int.MIN_VALUE
