7f0ef2cfb4cb0846c43bb9f19f8646d94a94ead5
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-06 09:50:26 -0400

    [Tour] - Adds extension method for identifying if the tour stop is intro stop

--- db/src/main/kotlin/edu/artic/db/Constants.kt
+++ db/src/main/kotlin/edu/artic/db/Constants.kt
@@ -10,3 +10,8 @@ package edu.artic.db
  * Since negative numbers are valid number for underground floors we are using Int.MIN_VALUE.
  */
 const val INVALID_FLOOR: Int = Int.MIN_VALUE
+
+/**
+ * Dummy id to represent the intro tour stop.
+ */
+const val INTRO_TOUR_STOP_OBJECT_ID: String = "INTRO"
