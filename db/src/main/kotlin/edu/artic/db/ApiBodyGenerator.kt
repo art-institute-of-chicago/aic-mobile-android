9d29d0997401db5aab254ac4e7c061e7882bc6d2
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-06 15:20:36 -0400

    [SearchApi] - Net: create request bodies for the various search query types

--- db/src/main/kotlin/edu/artic/db/ApiBodyGenerator.kt
+++ db/src/main/kotlin/edu/artic/db/ApiBodyGenerator.kt
@@ -64,6 +64,98 @@ sealed class ApiBodyGenerator {
             return postParams
         }
 
+        fun createSearchQueryBody(searchQuery: String): MutableList<MutableMap<String, Any>> {
+            val artworkParams = createSearchArtworkQueryBody(searchQuery)
+
+            val tourParams = createSearchTourQueryBody(searchQuery)
+
+
+            val exhibitionParams = createSearchExhibitionQueryBody(searchQuery)
+
+
+            return mutableListOf(artworkParams, tourParams, exhibitionParams)
+        }
+
+        fun createSearchArtworkQueryBody(searchQuery: String): MutableMap<String, Any> {
+            val artworkParams = mutableMapOf<String, Any>()
+
+            artworkParams["resources"] = "artworks"
+            artworkParams["from"] = 0
+            artworkParams["q"] = searchQuery
+            artworkParams["fields"] = listOf(
+                    "id",
+                    "is_on_view",
+                    "title",
+                    "artist_display",
+                    "image_id",
+                    "gallery_id",
+                    "latlon"
+            )
+            artworkParams["query"] = mutableMapOf<String, Any>().apply {
+                this["term"] = mutableMapOf<String, Any>().apply {
+                    this["is_on_view"] = "true"
+                }
+            }
+            return artworkParams
+        }
+
+        fun createSearchTourQueryBody(searchQuery: String): MutableMap<String, Any> {
+            val tourParams = mutableMapOf<String, Any>()
+
+            tourParams["resources"] = "tours"
+            tourParams["from"] = 0
+            tourParams["q"] = searchQuery
+            tourParams["fields"] = listOf(
+                    "id"
+            )
+            tourParams["size"] = 99
+            return tourParams
+        }
+
+        fun createSearchExhibitionQueryBody(searchQuery: String): MutableMap<String, Any> {
+            val exhibitionParams = mutableMapOf<String, Any>()
+
+            exhibitionParams["resources"] = "exhibitions"
+            exhibitionParams["from"] = 0
+            exhibitionParams["q"] = searchQuery
+            exhibitionParams["fields"] = listOf(
+                    "id",
+                    "title",
+                    "short_description",
+                    "legacy_image_mobile_url",
+                    "legacy_image_desktop_url",
+                    "gallery_id",
+                    "web_url",
+                    "aic_start_at",
+                    "aic_end_at"
+            )
+            exhibitionParams["size"] = 99
+            exhibitionParams["query"] = mutableMapOf<String, Any>().apply {
+                //Boolean map
+                this["bool"] = mutableMapOf<String, Any>().apply {
+                    this["must"] = mutableListOf<Any>().apply {
+                        this.add(mutableMapOf<String, Any>().apply {
+                            //range
+                            this["range"] = mutableMapOf<String, Any>().apply {
+                                this["aic_start_at"] = mutableMapOf<String, String>().apply {
+                                    this["lte"] = "now"
+                                }
+                            }
+                        })
+
+                        this.add(mutableMapOf<String, Any>().apply {
+                            this["range"] = mutableMapOf<String, Any>().apply {
+                                this["aic_end_at"] = mutableMapOf<String, String>().apply {
+                                    this["gte"] = "now"
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+            return exhibitionParams
+        }
+
         fun createEventQueryBody(): MutableMap<String, Any> {
             val postParams = mutableMapOf<String, Any>()
             postParams["fields"] = listOf(
