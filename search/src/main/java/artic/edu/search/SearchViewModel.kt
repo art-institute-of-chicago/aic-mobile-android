0c45109ed209f969a488c84aca4afcb1743fb47f
Piotr Leja <pleja@fuzzproductions.com>
2018-09-06 16:10:09 -0400

    [SearchFragment] return close button functionality that was omitted accidentally

--- search/src/main/java/artic/edu/search/SearchViewModel.kt
+++ search/src/main/java/artic/edu/search/SearchViewModel.kt
@@ -5,11 +5,16 @@ import edu.artic.analytics.AnalyticsTracker
 import edu.artic.analytics.ScreenCategoryName
 import edu.artic.viewmodel.NavViewViewModel
 import edu.artic.viewmodel.Navigate
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor(private val analyticsTracker: AnalyticsTracker,
                                           private val searchResultsManager: SearchResultsManager)
     : NavViewViewModel<SearchViewModel.NavigationEndpoint>() {
+
+    val closeButtonVisible : Subject<Boolean> = BehaviorSubject.createDefault(false)
+    val shouldClearTextInput : Subject<Boolean> = BehaviorSubject.createDefault(false)
 
     sealed class NavigationEndpoint {
         object DefaultSearchResults : NavigationEndpoint()
@@ -23,10 +28,18 @@ class SearchViewModel @Inject constructor(private val analyticsTracker: Analytic
     fun onTextChanged(newText: String) {
         //TODO pass new text to manager
         if (newText.isEmpty()) {
+            closeButtonVisible.onNext(false)
             navigateTo.onNext(Navigate.Forward(NavigationEndpoint.DefaultSearchResults))
         } else {
+            closeButtonVisible.onNext(true)
             navigateTo.onNext(Navigate.Forward(NavigationEndpoint.DynamicSearchResults))
         }
+    }
+
+    fun onCloseClicked() {
+        shouldClearTextInput.onNext(true)
+        shouldClearTextInput.onNext(false)
+        onTextChanged("")
     }
 
 }
