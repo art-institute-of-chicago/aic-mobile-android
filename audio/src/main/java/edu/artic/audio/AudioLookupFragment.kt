959680a21e595de8ce0cc8cceab68be9795bb81b
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-06 21:02:28 -0400

    [Nav] - AudioLookup: omit duplicate navigation events

--- audio/src/main/java/edu/artic/audio/AudioLookupFragment.kt
+++ audio/src/main/java/edu/artic/audio/AudioLookupFragment.kt
@@ -129,7 +129,7 @@ class AudioLookupFragment : BaseViewModelFragment<AudioLookupViewModel>() {
                             }
                         }
                     }
-                }.disposedBy(disposeBag)
+                }.disposedBy(navigationDisposeBag)
     }
 
 
