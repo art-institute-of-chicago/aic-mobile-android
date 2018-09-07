348c0e6ba86cb2685dff2a10aaea221f6cd462c8
Piotr Leja <pleja@fuzzproductions.com>
2018-09-07 11:11:37 -0400

    [Search] add ui and view models to Adapter for list tiems (AIC-70)(AIC-353)

--- search/src/main/java/artic/edu/search/SearchResultsAdapter.kt
+++ search/src/main/java/artic/edu/search/SearchResultsAdapter.kt
@@ -1,30 +1,56 @@
 package artic.edu.search
 
 import android.view.View
+import com.bumptech.glide.Glide
 import com.fuzz.rx.bindTo
 import com.fuzz.rx.bindToMain
 import com.fuzz.rx.disposedBy
+import com.jakewharton.rxbinding2.view.visibility
 import com.jakewharton.rxbinding2.widget.text
 import edu.artic.adapter.AutoHolderRecyclerViewAdapter
 import kotlinx.android.synthetic.main.layout_cell_result_header.view.*
+import kotlinx.android.synthetic.main.layout_cell_search_list_item.view.*
 import kotlinx.android.synthetic.main.layout_cell_suggested_keyword.view.*
 
 class SearchResultsAdapter : AutoHolderRecyclerViewAdapter<SearchResultBaseCellViewModel>() {
 
     override fun View.onBindView(item: SearchResultBaseCellViewModel, position: Int) {
         when (item) {
+
             is SearchResultHeaderCellViewModel -> {
                 item.text
                         .bindToMain(title.text())
                         .disposedBy(item.viewDisposeBag)
             }
-            is SearchResultArtworkCellViewModel -> {
 
-            }
-            is SearchResultExhibitionCellViewModel -> {
+            is SearchResultBaseListItemViewModel -> {
+                item.imageUrl
+                        .subscribe {
+                            Glide.with(context)
+                                    .load(it)
+                                    .into(image)
+                        }.disposedBy(item.viewDisposeBag)
+                item.isHeadphonesVisisble
+                        .bindToMain(headphonesIcon.visibility())
+                        .disposedBy(item.viewDisposeBag)
 
-            }
-            is SearchResultTourCellViewModel -> {
+                item.itemTitle
+                        .bindToMain(itemTitle.text())
+                        .disposedBy(item.viewDisposeBag)
+                item.itemSubTitle
+                        .bindToMain(itemSubTitle.text())
+                        .disposedBy(item.viewDisposeBag)
+
+                when (item) {
+                    is SearchResultArtworkCellViewModel -> {
+
+                    }
+                    is SearchResultExhibitionCellViewModel -> {
+
+                    }
+                    is SearchResultTourCellViewModel -> {
+                    }
+                }
             }
             is SearchResultTextCellViewModel -> {
                 item.text
@@ -41,9 +67,9 @@ class SearchResultsAdapter : AutoHolderRecyclerViewAdapter<SearchResultBaseCellV
         val item = getItem(position)
         return when (item) {
             is SearchResultHeaderCellViewModel -> R.layout.layout_cell_result_header
-            is SearchResultArtworkCellViewModel -> 0
-            is SearchResultExhibitionCellViewModel -> 0
-            is SearchResultTourCellViewModel -> 0
+            is SearchResultArtworkCellViewModel -> R.layout.layout_cell_search_list_item
+            is SearchResultExhibitionCellViewModel -> R.layout.layout_cell_search_list_item
+            is SearchResultTourCellViewModel -> R.layout.layout_cell_search_list_item
             is SearchResultTextCellViewModel -> R.layout.layout_cell_suggested_keyword
             is SearchResultEmptyCellViewModel -> 0
             else -> R.layout.layout_cell_suggested_map_object
