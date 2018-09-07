4530d49eb246903e0b89f8179f762990616e8ad1
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-07 12:08:42 -0400

    [Util] - Result: add function for mapping api errors to default values

---
+++ search/src/main/java/artic/edu/search/ObservableExtensions.kt
@@ -0,0 +1,22 @@
+package artic.edu.search
+
+import com.fuzz.retrofit.rx.requireValue
+import com.jakewharton.retrofit2.adapter.rxjava2.Result
+import io.reactivex.Observable
+
+/**
+ * Perform a simple [map] call over the emitted [Result]s.
+ *
+ * - If the api call [resulted in failure][Result.isError], we map to [onErrorValue].
+ * - Else, if the api call [succeeded][com.fuzz.retrofit.rx.isSuccess] we map to its
+ * [internal content][Result.requireValue].
+ */
+fun <T> Observable<Result<T>>.mapWithDefault(onErrorValue : T) : Observable<T> {
+    return map {
+        if (it.isError) {
+            onErrorValue
+        } else {
+            it.requireValue()
+        }
+    }
+}
