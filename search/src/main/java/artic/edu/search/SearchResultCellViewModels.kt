13ef874ba698ddf64168af54a8df78650de09ded
Piotr Leja <pleja@fuzzproductions.com>
2018-09-07 12:17:30 -0400

    [SearchResults] add flows to handle exhibition tours and artwork (AIC-353)(AIC-70)

--- search/src/main/java/artic/edu/search/SearchResultCellViewModels.kt
+++ search/src/main/java/artic/edu/search/SearchResultCellViewModels.kt
@@ -1,5 +1,9 @@
 package artic.edu.search
 
+import android.support.annotation.DrawableRes
+import edu.artic.db.models.ArticExhibition
+import edu.artic.db.models.ArticTour
+import edu.artic.ui.util.asCDNUri
 import edu.artic.viewmodel.BaseViewModel
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
@@ -16,17 +20,52 @@ class SearchResultHeaderCellViewModel(text: String) : SearchResultBaseCellViewMo
     val text: Subject<String> = BehaviorSubject.createDefault(text)
 }
 
+class SearchResultOnMapHeaderCellViewModel(text: String) : SearchResultBaseCellViewModel() {
+    val text: Subject<String> = BehaviorSubject.createDefault(text)
+}
+
 open class SearchResultBaseListItemViewModel(isHeadphonesVisisble: Boolean = false)
     : SearchResultBaseCellViewModel(hasDivider = true) {
     val imageUrl: Subject<String> = BehaviorSubject.create()
     val itemTitle: Subject<String> = BehaviorSubject.create()
     val itemSubTitle: Subject<String> = BehaviorSubject.create()
-    val isHeadphonesVisisble: Subject<Boolean> = BehaviorSubject.createDefault(isHeadphonesVisisble)
+    val isHeadphonesVisible: Subject<Boolean> = BehaviorSubject.createDefault(isHeadphonesVisisble)
 }
 
 class SearchResultArtworkCellViewModel
     : SearchResultBaseListItemViewModel(isHeadphonesVisisble = true)
 
-class SearchResultExhibitionCellViewModel : SearchResultBaseListItemViewModel()
+class SearchResultExhibitionCellViewModel (val articExhibition: ArticExhibition) : SearchResultBaseListItemViewModel() {
+    init {
+        imageUrl.onNext(articExhibition.legacy_image_mobile_url.orEmpty())
+        itemTitle.onNext(articExhibition.title)
+    }
+}
 
-class SearchResultTourCellViewModel : SearchResultBaseListItemViewModel()
+class SearchResultTourCellViewModel(val articTour: ArticTour) : SearchResultBaseListItemViewModel() {
+    init {
+        imageUrl.onNext(articTour.thumbUrl.orEmpty())
+        itemTitle.onNext(articTour.title)
+    }
+}
+
+/**
+ * ViewModel for displaying the circular artwork image under "On the map" section.
+ */
+class SearchResultCircularCellViewModel(val artWork: SearchViewComponent.Artwork?) : SearchResultBaseCellViewModel() {
+
+    val imageUrl: Subject<String> = BehaviorSubject.create()
+
+    init {
+        artWork?.value?.thumbnailFullPath?.asCDNUri()?.let { url ->
+            imageUrl.onNext(url)
+        }
+
+    }
+
+}
+
+/**
+ * ViewModel for displaying the amenities icons
+ */
+class SearchResultAmenitiesCellViewModel(@DrawableRes val value: Int) : SearchResultBaseCellViewModel()
--- search/src/main/java/artic/edu/search/SearchResultsSuggestedViewModel.kt
+++ search/src/main/java/artic/edu/search/SearchResultsSuggestedViewModel.kt
@@ -2,23 +2,104 @@ package artic.edu.search
 
 import com.fuzz.rx.bindTo
 import com.fuzz.rx.disposedBy
+import edu.artic.db.daos.ArticObjectDao
+import edu.artic.db.daos.ArticSearchObjectDao
 import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
-class SearchResultsSuggestedViewModel @Inject constructor(manager: SearchResultsManager) : BaseViewModel() {
+class SearchResultsSuggestedViewModel @Inject constructor(private val manager: SearchResultsManager,
+                                                          private val searchSuggestionsDao: ArticSearchObjectDao,
+                                                          private val objectDao: ArticObjectDao)
+    : BaseViewModel() {
 
     val cells: Subject<List<SearchResultBaseCellViewModel>> = BehaviorSubject.create()
-
+    private val dynamicCells: Subject<List<SearchResultBaseCellViewModel>> = BehaviorSubject.create()
+    private val suggestedArtworks: Subject<List<SearchResultCircularCellViewModel>> = BehaviorSubject.create()
 
     init {
-        manager.currentSearchResults
-                .map {
-                    it.suggestions.map { SearchResultTextCellViewModel(it) }
-                }.bindTo(cells)
+        setupOnMapSuggestionsBind()
+
+        setupResultsBind()
+
+        Observables
+                .combineLatest(
+                        dynamicCells,
+                        Observable.just(listOf(
+                                SearchResultAmenitiesCellViewModel(R.drawable.ic_icon_amenity_map_restaurant),
+                                SearchResultAmenitiesCellViewModel(R.drawable.ic_icon_amenity_map_lounge),
+                                SearchResultAmenitiesCellViewModel(R.drawable.ic_icon_amenity_map_shop),
+                                SearchResultAmenitiesCellViewModel(R.drawable.ic_icon_amenity_map_restroom))
+                        ),
+                        suggestedArtworks)
+                { dynamicCells, amenities, suggestedArtworks ->
+                    return@combineLatest mutableListOf<SearchResultBaseCellViewModel>().apply {
+                        addAll(dynamicCells)
+                        add(SearchResultOnMapHeaderCellViewModel("On the Map"))
+                        addAll(amenities)
+                        addAll(suggestedArtworks)
+                    }
+                }
+                .bindTo(cells)
                 .disposedBy(disposeBag)
 
+    }
+
+    private fun setupOnMapSuggestionsBind() {
+        searchSuggestionsDao.getDataObject()
+                .toObservable()
+                .map { suggestedSearchOptions -> suggestedSearchOptions.searchObjects }
+                .flatMap { idsList ->
+                    objectDao.getObjectsByIdList(idsList).toObservable()
+                }
+                .map { objects ->
+                    objects.map {
+                        SearchResultCircularCellViewModel(SearchViewComponent.Artwork(it))
+                    }
+                }
+                .bindTo(suggestedArtworks)
+                .disposedBy(disposeBag)
+    }
+
+    private fun setupResultsBind() {
+        manager.currentSearchResults
+                .map { result ->
+                    val cellList = mutableListOf<SearchResultBaseCellViewModel>()
+
+                    cellList.addAll(result.suggestions.map { SearchResultTextCellViewModel(it) })
+                    if (result.artworks.isNotEmpty()) {
+                        //TODO: add header
+                        cellList.addAll(
+                                result.artworks
+                                        .take(3)
+                                        .map { SearchResultArtworkCellViewModel() }
+                        )
+                    }
+                    if (result.exhibitions.isNotEmpty()) {
+                        cellList.add(SearchResultHeaderCellViewModel("Exhibitions"))
+                        cellList.addAll(
+                                result.exhibitions
+                                        .take(3)
+                                        .map { SearchResultExhibitionCellViewModel(it) }
+                        )
+                    }
+
+                    if (result.tours.isNotEmpty()) {
+                        //TODO: add header
+                        cellList.addAll(
+                                result.exhibitions
+                                        .take(3)
+                                        .map { SearchResultExhibitionCellViewModel(it) }
+                        )
+                    }
+
+                    return@map cellList
+
+                }.bindTo(dynamicCells)
+                .disposedBy(disposeBag)
     }
 
 }
