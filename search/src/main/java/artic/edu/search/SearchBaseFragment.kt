9085f67d1e268c1f99f3e14d5a795e73ac0e4223
Piotr Leja <pleja@fuzzproductions.com>
2018-09-11 10:09:48 -0400

    [Search] refactor common functionality into base class

--- search/src/main/java/artic/edu/search/SearchBaseFragment.kt
+++ search/src/main/java/artic/edu/search/SearchBaseFragment.kt
@@ -1,16 +1,21 @@
 package artic.edu.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.view.View
 import com.fuzz.rx.bindToMain
 import com.fuzz.rx.disposedBy
 import edu.artic.adapter.itemChanges
+import edu.artic.adapter.itemClicksWithPosition
 import edu.artic.analytics.ScreenCategoryName
+import edu.artic.base.utils.asDeepLinkIntent
+import edu.artic.navigation.NavigationConstants
 import edu.artic.viewmodel.BaseViewModelFragment
+import edu.artic.viewmodel.Navigate
 import kotlinx.android.synthetic.main.fragment_search_results_sub.*
 
-abstract class SearchBaseFragment<TViewModel : SearchBaseViewModel<*>> : BaseViewModelFragment<TViewModel>() {
+abstract class SearchBaseFragment<TViewModel : SearchBaseViewModel> : BaseViewModelFragment<TViewModel>() {
     override val title: String = ""
     override val layoutResId: Int = R.layout.fragment_search_results_sub
     override val screenCategory: ScreenCategoryName? = null
@@ -44,6 +49,31 @@ abstract class SearchBaseFragment<TViewModel : SearchBaseViewModel<*>> : BaseVie
                 .bindToMain(adapter.itemChanges())
                 .disposedBy(disposeBag)
 
+        adapter.itemClicksWithPosition()
+                .subscribe { (pos, vm) ->
+                    viewModel.onClickItem(pos, vm)
+                }.disposedBy(disposeBag)
+
+    }
+
+    override fun setupNavigationBindings(viewModel: TViewModel) {
+        super.setupNavigationBindings(viewModel)
+        viewModel.navigateTo
+                .filter { it is Navigate.Forward }
+                .map { it as Navigate.Forward }
+                .subscribe {
+                    when(it.endpoint) {
+                        is SearchBaseViewModel.NavigationEndpoint.ArticObjectDetails -> {
+                            val o = (it.endpoint as SearchBaseViewModel.NavigationEndpoint.ArticObjectDetails).articObject
+                            val mapIntent = NavigationConstants.MAP.asDeepLinkIntent().apply {
+                                putExtra(NavigationConstants.ARG_SEARCH_OBJECT, o)
+                                flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT or Intent.FLAG_ACTIVITY_NO_ANIMATION
+                            }
+                            startActivity(mapIntent)
+                        }
+                    }
+                }
+                .disposedBy(navigationDisposeBag)
     }
 
 }
--- search/src/main/java/artic/edu/search/SearchBaseViewModel.kt
+++ search/src/main/java/artic/edu/search/SearchBaseViewModel.kt
@@ -1,13 +1,56 @@
 package artic.edu.search
 
+import edu.artic.analytics.AnalyticsTracker
+import edu.artic.db.models.ArticObject
 import edu.artic.viewmodel.NavViewViewModel
+import edu.artic.viewmodel.Navigate
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
-open class SearchBaseViewModel<NavEndpoint> @Inject constructor()
-    : NavViewViewModel<NavEndpoint>() {
+open class SearchBaseViewModel @Inject constructor(protected val analyticsTracker: AnalyticsTracker)
+    : NavViewViewModel<SearchBaseViewModel.NavigationEndpoint>() {
+
+    sealed class NavigationEndpoint {
+        data class ArticObjectDetails(val articObject: ArticObject) : NavigationEndpoint()
+    }
 
     val cells: Subject<List<SearchBaseCellViewModel>> = BehaviorSubject.create()
 
+
+    fun onClickItem(pos: Int, viewModel: SearchBaseCellViewModel) {
+        when (viewModel) {
+            is SearchTourCellViewModel -> {
+
+            }
+            is SearchExhibitionCellViewModel -> {
+
+            }
+            is SearchArtworkCellViewModel -> {
+
+            }
+            is SearchAmenitiesCellViewModel -> {
+
+            }
+            is SearchEmptyCellViewModel -> {
+
+            }
+            is SearchCircularCellViewModel -> {
+                viewModel.artWork?.let { articObject ->
+                    navigateTo.onNext(
+                            Navigate.Forward(
+                                    NavigationEndpoint.ArticObjectDetails(articObject)
+                            )
+                    )
+
+                }
+            }
+            is SearchTextCellViewModel -> {
+
+            }
+            is SearchHeaderCellViewModel -> {
+
+            }
+        }
+    }
 }
