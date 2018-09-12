9d99ec06d183e6900acce73a2e057fc7fd158468
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-12 16:23:48 -0400

    [Info] - Adds join now feature

--- info/src/main/java/edu/artic/info/InformationFragment.kt
+++ info/src/main/java/edu/artic/info/InformationFragment.kt
@@ -1,15 +1,25 @@
 package edu.artic.info
 
+import android.net.Uri
+import com.fuzz.rx.bindTo
+import com.fuzz.rx.defaultThrottle
 import com.fuzz.rx.disposedBy
+import com.jakewharton.rxbinding2.view.clicks
 import edu.artic.analytics.ScreenCategoryName
+import edu.artic.base.utils.customTab.CustomTabManager
 import edu.artic.viewmodel.BaseViewModelFragment
 import edu.artic.viewmodel.Navigate
 import io.reactivex.functions.Consumer
+import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.fragment_information.*
+import javax.inject.Inject
 import kotlin.reflect.KClass
 
 
 class InformationFragment : BaseViewModelFragment<InformationViewModel>() {
+
+    @Inject
+    lateinit var customTabManager : CustomTabManager
 
     override val screenCategory: ScreenCategoryName
         get() = ScreenCategoryName.Information
@@ -28,18 +38,28 @@ class InformationFragment : BaseViewModelFragment<InformationViewModel>() {
     override fun setupBindings(viewModel: InformationViewModel) {
         super.setupBindings(viewModel)
         appBarLayout.setOnSearchClickedConsumer(Consumer { viewModel.onClickSearch() })
+
+        joinNow.clicks()
+                .defaultThrottle()
+                .subscribeBy {
+                    viewModel.joinNow()
+                }.disposedBy(disposeBag)
     }
 
     override fun setupNavigationBindings(viewModel: InformationViewModel) {
         super.setupNavigationBindings(viewModel)
         viewModel.navigateTo
                 .subscribe {
-                    when(it) {
+                    when (it) {
                         is Navigate.Forward -> {
-                            when(it.endpoint) {
+                            when (it.endpoint) {
                                 InformationViewModel.NavigationEndpoint.AccessMemberCard -> TODO()
                                 InformationViewModel.NavigationEndpoint.Search -> {
                                     navController.navigate(R.id.goToSearch)
+                                }
+                                is InformationViewModel.NavigationEndpoint.JoinNow -> {
+                                    val url = (it.endpoint as InformationViewModel.NavigationEndpoint.JoinNow).url
+                                    customTabManager.openUrlOnChromeCustomTab(requireContext(), Uri.parse(resources.getString(url)))
                                 }
                             }
                         }
