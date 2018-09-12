7680da5dd36c792a6ee1c8af5ebe9490dfc4f914
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-12 17:08:37 -0400

    [Info] - MemberShip : Pulls membership information from dataObjectDao

--- info/src/main/java/edu/artic/info/InformationViewModel.kt
+++ info/src/main/java/edu/artic/info/InformationViewModel.kt
@@ -1,9 +1,11 @@
 package edu.artic.info
 
-import android.support.annotation.StringRes
+import com.fuzz.rx.bindTo
+import com.fuzz.rx.disposedBy
 import edu.artic.analytics.AnalyticsAction
 import edu.artic.analytics.AnalyticsTracker
 import edu.artic.analytics.ScreenCategoryName
+import edu.artic.db.daos.ArticDataObjectDao
 import edu.artic.viewmodel.NavViewViewModel
 import edu.artic.viewmodel.Navigate
 import javax.inject.Inject
@@ -11,20 +13,26 @@ import javax.inject.Inject
 /**
  * @author Sameer Dhakal (Fuzz)
  */
-class InformationViewModel @Inject constructor(val analyticsTracker: AnalyticsTracker) : NavViewViewModel<InformationViewModel.NavigationEndpoint>() {
+class InformationViewModel @Inject constructor(val analyticsTracker: AnalyticsTracker, val dataObjectDao: ArticDataObjectDao) : NavViewViewModel<InformationViewModel.NavigationEndpoint>() {
 
     sealed class NavigationEndpoint {
         object AccessMemberCard : NavigationEndpoint()
         object Search : NavigationEndpoint()
-        class JoinNow(@StringRes val url: Int) : NavigationEndpoint()
+        class JoinNow(val url: String) : NavigationEndpoint()
     }
 
     fun onClickSearch() {
         navigateTo.onNext(Navigate.Forward(NavigationEndpoint.Search))
     }
 
-    fun joinNow() {
+    fun onClickJoinNow() {
         analyticsTracker.reportEvent(ScreenCategoryName.Information, AnalyticsAction.memberJoinPressed)
-        navigateTo.onNext(Navigate.Forward(NavigationEndpoint.JoinNow(R.string.joinUrl)))
+        dataObjectDao.getDataObject()
+                .toObservable()
+                .map { it.membershipUrl.orEmpty() }
+                .filter { it.isNotEmpty() }
+                .map { Navigate.Forward(NavigationEndpoint.JoinNow(it)) }
+                .bindTo(navigateTo)
+                .disposedBy(disposeBag)
     }
 }
