46e7deefc011aec76c2b6e43c8c10a4b78b5d6f7
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-12 19:27:35 -0400

    [Database] - Update schema version to account for ArticExhibition changes

--- db/src/main/kotlin/edu/artic/db/AppDatabase.kt
+++ db/src/main/kotlin/edu/artic/db/AppDatabase.kt
@@ -22,7 +22,7 @@ import edu.artic.db.models.*
             ArticMapFloor::class,
             ArticSearchSuggestionsObject::class
         ],
-        version = 2,
+        version = 3,
         exportSchema = false
 )
 @TypeConverters(AppConverters::class)
