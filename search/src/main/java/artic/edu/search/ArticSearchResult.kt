4db2389cc2c188d4c79af481774d5c30f17d354b
Piotr Leja <pleja@fuzzproductions.com>
2018-09-13 18:32:42 -0400

    [Search] use new Artic object and generate model from needed parts (AIC-377)

--- search/src/main/java/artic/edu/search/ArticSearchResult.kt
+++ search/src/main/java/artic/edu/search/ArticSearchResult.kt
@@ -2,12 +2,13 @@ package artic.edu.search
 
 import edu.artic.db.models.ArticExhibition
 import edu.artic.db.models.ArticObject
+import edu.artic.db.models.ArticSearchArtworkObject
 import edu.artic.db.models.ArticTour
 
 data class ArticSearchResult(
         var searchTerm : String,
         var suggestions: List<String>,
-        val artworks: List<ArticObject>,
+        val artworks: List<ArticSearchArtworkObject>,
         val tours: List<ArticTour>,
         val exhibitions: List<ArticExhibition>
 )
