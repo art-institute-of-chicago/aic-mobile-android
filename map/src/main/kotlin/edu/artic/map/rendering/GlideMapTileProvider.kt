706ae3b8a311a5f0c53a76967e467ebad085cb54
Piotr Leja <pleja@fuzzproductions.com>
2018-09-13 12:57:51 -0400

    [Map] temporarily convert to jpg on app side for tiling (AIC-383)

--- map/src/main/kotlin/edu/artic/map/rendering/GlideMapTileProvider.kt
+++ map/src/main/kotlin/edu/artic/map/rendering/GlideMapTileProvider.kt
@@ -1,9 +1,12 @@
 package edu.artic.map.rendering
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import com.bumptech.glide.Glide
 import com.google.android.gms.maps.model.Tile
 import timber.log.Timber
+import java.io.ByteArrayOutputStream
 import java.io.IOException
 import kotlin.math.pow
 
@@ -25,9 +28,18 @@ class GlideMapTileProvider(private val context: Context,
 
                 val path = "http://aic-mobile-tours.artic.edu/sites/default/files/floor-maps/tiles/floor$floor/zoom$zoom/tiles-$tileNumber.png"
                 return try {
+                    val bitmap = Glide.with(context).asBitmap().load(path).submit(TILE_SIZE.toInt(), TILE_SIZE.toInt()).get()
+                    val newBitmap = bitmap.copy(Bitmap.Config.RGB_565, true)
+                     val stream = ByteArrayOutputStream()
+                    newBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+                    val array = stream.toByteArray()
+                    stream.reset()
+                    newBitmap.recycle()
+                    // The above flow is temporary until we get JPEG's provided from endpoint,
+                    // which will require an update here.
                     Tile(TILE_SIZE.toInt(),
                             TILE_SIZE.toInt(),
-                            Glide.with(context).`as`(ByteArray::class.java).load(path).submit().get()
+                            array
                     )
                 } catch (e: IOException) {
                     Timber.e("Could not find tile with $path")
