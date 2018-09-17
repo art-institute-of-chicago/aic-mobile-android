5ac5b256892e13c47b1479c033446cb8cbfa9fed
Piotr Leja <pleja@fuzzproductions.com>
2018-09-17 15:57:38 -0400

    [Search] update UI for headers (AIC-378)

--- search/src/main/java/edu/artic/search/SearchResultsContainerFragment.kt
+++ search/src/main/java/edu/artic/search/SearchResultsContainerFragment.kt
@@ -1,7 +1,13 @@
 package edu.artic.search
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v4.view.ViewPager
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import edu.artic.analytics.ScreenCategoryName
 import edu.artic.viewmodel.BaseViewModelFragment
 import kotlinx.android.synthetic.main.fragment_search_results.*
@@ -17,9 +23,45 @@ class SearchResultsContainerFragment : BaseViewModelFragment<SearchResultsContai
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tabStrip.setupWithViewPager(viewPager)
         val adapter = SearchResultsPagerAdapter(childFragmentManager)
         viewPager.adapter = adapter
         viewPager.currentItem = 0
         viewPager.offscreenPageLimit = 1
+        setupTabStripUi()
+    }
+
+    private fun setupTabStripUi() {
+        val selectedTypeface = ResourcesCompat.getFont(requireContext(), R.font.ideal_sans_medium)
+        val defaultTypeface = ResourcesCompat.getFont(requireContext(), R.font.ideal_sans_light)
+
+        for (i in 0 until tabStrip.tabCount) {
+            tabStrip.getTabAt(i)?.let {
+                it.setCustomView(R.layout.tab_search_result_container)
+                (it.customView as TextView).apply {
+                    text = viewPager.adapter?.getPageTitle(i)
+                    typeface = if(i == 0) {
+                        selectedTypeface
+                    } else {
+                        defaultTypeface
+                    }
+                }
+            }
+        }
+        tabStrip.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                (tab?.customView as TextView?)?.typeface = selectedTypeface
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                (tab?.customView as TextView?)?.typeface = defaultTypeface
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                (tab?.customView as TextView?)?.typeface = selectedTypeface
+            }
+
+        })
     }
 }
