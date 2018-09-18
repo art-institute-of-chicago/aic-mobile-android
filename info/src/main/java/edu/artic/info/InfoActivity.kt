6cd5a501c48eb9d2d30c4c50aae5b2f3a82ebb75
Piotr Leja <pleja@fuzzproductions.com>
2018-09-18 13:36:06 -0400

    [LocationSettings] add flow from information page to location settings page (AIC-68)(AIC-136)

--- info/src/main/java/edu/artic/info/InfoActivity.kt
+++ info/src/main/java/edu/artic/info/InfoActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import edu.artic.navigation.NavigationSelectListener
 import edu.artic.base.utils.disableShiftMode
 import edu.artic.base.utils.preventReselection
+import edu.artic.location.LocationService
 import edu.artic.ui.BaseActivity
 import kotlinx.android.synthetic.main.activity_info.*
+import javax.inject.Inject
 
 class InfoActivity : BaseActivity() {
+
+    /**
+     * locationService is here so it gets generated before the onResume flow happens
+     */
+    @Inject
+    lateinit var locationService: LocationService
 
     override val layoutResId: Int
         get() = R.layout.activity_info
