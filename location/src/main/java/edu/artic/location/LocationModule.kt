0af9887fd71b32762214bca78863ad19bfb67229
Piotr Leja <pleja@fuzzproductions.com>
2018-09-18 10:52:30 -0400

    [Location] create basis for dagger LocationModule (AIC-68)(AIC-442)

---
+++ location/src/main/java/edu/artic/location/LocationModule.kt
@@ -0,0 +1,30 @@
+package edu.artic.location
+
+import dagger.Module
+
+/**
+ * @author Piotr Leja (Fuzz)
+ */
+@Module
+abstract class LocationModule {
+
+//    @Binds
+//    @IntoMap
+//    @ViewModelKey(SearchViewModel::class)
+//    abstract fun searchViewModel(splashViewModel: SearchViewModel): ViewModel
+//
+//    @get:ContributesAndroidInjector
+//    abstract val searchFragment: SearchFragment
+//
+//    @Module
+//    companion object {
+//
+//        @JvmStatic
+//        @Provides
+//        @Singleton
+//        fun provideSearchService(
+//                @Named(ApiModule.RETROFIT_BLOB_API) retrofit: Retrofit,
+//                dataObjectDao: ArticDataObjectDao
+//        ): SearchServiceProvider = RetrofitSearchServiceProvider(retrofit, dataObjectDao)
+//    }
+}
