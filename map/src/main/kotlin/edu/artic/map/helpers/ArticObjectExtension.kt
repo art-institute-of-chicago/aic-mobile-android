1ce3ceaeaaf62b0443d2bcf365cbe094da55b95e
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-18 13:05:20 -0400

    [Util] - Add conversion method to get LatLng from ArticSearchArtworkObjects

--- map/src/main/kotlin/edu/artic/map/helpers/ArticObjectExtension.kt
+++ map/src/main/kotlin/edu/artic/map/helpers/ArticObjectExtension.kt
@@ -2,12 +2,20 @@ package edu.artic.map.helpers
 
 import com.google.android.gms.maps.model.LatLng
 import edu.artic.db.models.ArticObject
+import edu.artic.db.models.ArticSearchArtworkObject
 import edu.artic.db.models.ArticTour
 
 /**
  * See [convertToLatLng].
  */
 fun ArticObject.toLatLng(): LatLng {
+    return convertToLatLng(location)
+}
+
+/**
+ * See [convertToLatLng].
+ */
+fun ArticSearchArtworkObject.toLatLng(): LatLng {
     return convertToLatLng(location)
 }
 
