8ffe244271f76e2c482a24a0c791f6745a3bb663
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-19 16:07:50 -0400

    [Language] - Adds language settings screen

---
+++ info/src/main/java/edu/artic/info/LanguageSettingsViewModel.kt
@@ -0,0 +1,48 @@
+package edu.artic.info
+
+import com.fuzz.rx.disposedBy
+import edu.artic.localization.LanguageSelector
+import edu.artic.localization.LocalizationPreferences
+import edu.artic.viewmodel.BaseViewModel
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+import java.util.*
+import javax.inject.Inject
+
+/**
+ * @author Sameer Dhakal (Fuzz)
+ */
+class LanguageSettingsViewModel @Inject constructor(
+        val languageSelector: LanguageSelector
+) : BaseViewModel() {
+
+
+    val selectedLanguage: Subject<Locale> = BehaviorSubject.create()
+
+    init {
+
+        selectedLanguage.onNext(languageSelector.getAppLocale())
+
+        selectedLanguage
+                .distinctUntilChanged()
+                .subscribe {
+                    languageSelector.setDefaultLanguageForApplication(it)
+
+                }.disposedBy(disposeBag)
+
+    }
+
+
+    fun onEnglishLanguageSelected() {
+        selectedLanguage.onNext(Locale.ENGLISH)
+    }
+
+    fun onSpanishLanguageSelected() {
+        selectedLanguage.onNext(Locale("es"))
+    }
+
+    fun onChineseLanguageSelected() {
+        selectedLanguage.onNext(Locale.CHINESE)
+    }
+
+}
