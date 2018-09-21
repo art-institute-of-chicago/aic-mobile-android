dacf58439261f776fc980abd517bfd59c7d800bd
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-21 16:53:26 -0400

    [Documentation] - Info: add docs to MemberDataProvider.kt

--- info/src/main/java/edu/artic/info/MemberDataProvider.kt
+++ info/src/main/java/edu/artic/info/MemberDataProvider.kt
@@ -6,6 +6,22 @@ import io.reactivex.Observable
  * @author Sameer Dhakal (Fuzz)
  */
 
+/**
+ * Contract between
+ * * a background service that calls an API and
+ * * the ViewModel layer ([InformationViewModel], perhaps)
+ *
+ * Implementors are encouraged to delegate their work to an instance
+ * of [MemberInfoApi].
+ */
 interface MemberDataProvider {
+    /**
+     * Retrieve information about the given member's membership. Both parameters
+     * should be non-empty.
+     *
+     * Note that this assumes that the [API][MemberInfoApi] communicates solely
+     * via `XML`; more formally, that it follows well-defined `SOAP` (Simple
+     * Object Access Protocol).
+     */
     fun getMemberData(memberID: String, zipCode: String): Observable<SOAPMemberInfoResponse>
 }
