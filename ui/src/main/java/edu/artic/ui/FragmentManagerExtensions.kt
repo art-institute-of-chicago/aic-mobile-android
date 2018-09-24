f1e537f9d4d31c5b272e49dbafa1a99a44110939
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-24 15:20:28 -0400

    [Nav] - Media Ui: detect audio player in more situations (AIC-441)

--- ui/src/main/java/edu/artic/ui/FragmentManagerExtensions.kt
+++ ui/src/main/java/edu/artic/ui/FragmentManagerExtensions.kt
@@ -1,5 +1,7 @@
 package edu.artic.ui
 
+import android.support.annotation.IdRes
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,4 +17,23 @@ import androidx.navigation.fragment.NavHostFragment
 fun FragmentManager.findNavController(): NavController? {
     val navFragment = primaryNavigationFragment
     return (navFragment as? NavHostFragment)?.navController
+}
+
+/**
+ * Finds a fragment of type `T` within the hierarchy anchored at [fm] with id [fragmentId].
+ *
+ * Returns null if there is no such fragment.
+ */
+inline fun <reified T : BaseFragment> findFragmentInHierarchy(fm: FragmentManager, @IdRes fragmentId: Int): T? {
+    var found: T? = null
+    var potential: Fragment?
+    var manager: FragmentManager? = fm
+    do {
+        potential = manager?.findFragmentById(fragmentId)
+        if (potential is T) {
+            found = potential
+        }
+        manager = manager?.primaryNavigationFragment?.childFragmentManager
+    } while (found == null && manager != null)
+    return found
 }
