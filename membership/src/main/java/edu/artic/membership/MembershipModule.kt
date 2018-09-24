c07d22628dfeb4967ad0f0ed3b6fa31d63173b96
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-24 15:40:00 -0400

    [Refactor] - Membership: Updates retrofit factory method

--- membership/src/main/java/edu/artic/membership/MembershipModule.kt
+++ membership/src/main/java/edu/artic/membership/MembershipModule.kt
@@ -47,26 +47,35 @@ abstract class MembershipModule {
         @Provides
         @Singleton
         @Named(MEMBER_INFO_API)
-        fun provideRetrofit(
-                @Named(ApiModule.BLOB_CLIENT_API)
-                client: OkHttpClient,
-                executor: Executor
-        ): Retrofit = constructRetrofit(
-                baseUrl = BuildConfig.MEMBER_INFO_API,
-                client = client,
-                executor = executor,
-                customConfiguration = {
-                    addConverterFactory(SimpleXmlConverterFactory.createNonStrict(
-                            Persister(AnnotationStrategy())
-                    ))
-                })
+        fun provideRetrofit(@Named(ApiModule.BLOB_CLIENT_API)
+                            client: OkHttpClient,
+                            executor: Executor)
+                : Retrofit? {
+            return if (BuildConfig.MEMBER_INFO_API.contains("https")) {
+                constructRetrofit(
+                        baseUrl = BuildConfig.MEMBER_INFO_API,
+                        client = client,
+                        executor = executor,
+                        customConfiguration = {
+                            addConverterFactory(SimpleXmlConverterFactory.createNonStrict(
+                                    Persister(AnnotationStrategy())
+                            ))
+                        })
+            } else {
+                null
+            }
+        }
 
         @JvmStatic
         @Provides
         @Singleton
-        fun provideMemberInfoService(
-                @Named(MEMBER_INFO_API) retrofit: Retrofit
-        ) = RetrofitMemberDataProvider(retrofit)
+        fun provideMemberInfoService(@Named(MEMBER_INFO_API) retrofit: Retrofit?): MemberDataProvider {
+            return if (retrofit == null) {
+                NoContentMemberDataProvider
+            } else {
+                RetrofitMemberDataProvider(retrofit)
+            }
+        }
 
         const val MEMBER_INFO_API = "MEMBER_INFO_API"
     }
