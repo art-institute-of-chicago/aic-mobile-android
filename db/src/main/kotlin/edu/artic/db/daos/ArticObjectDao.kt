fee25e61e80631d133a585fe7d46b69f226f39b4
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-24 10:24:25 -0400

    [Database] - Add methods to detect whether an object is in the db

--- db/src/main/kotlin/edu/artic/db/daos/ArticObjectDao.kt
+++ db/src/main/kotlin/edu/artic/db/daos/ArticObjectDao.kt
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
+import android.support.annotation.WorkerThread
 import edu.artic.db.models.ArticObject
 import io.reactivex.Flowable
 
@@ -16,14 +17,21 @@ interface ArticObjectDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addObjects(objects: List<ArticObject>)
 
-    //Must be flowable due to need to use this in 2 places
+    //Must be Flowable due to need to use this in 2 places
     @Query("select * from ArticObject where nid = :id")
     fun getObjectById(id: String): Flowable<ArticObject>
 
 
-    //Must be flowable due to need to use this in 2 places
+    //This one is intended for use when we don't want to filter out null objects
     @Query("select * from ArticObject where id = :id")
     fun getObjectByIdSynchronously(id: String): ArticObject?
+
+    /**
+     * Returns 1 (or more) if the object exists in the database, 0 (or less) if
+     * it's not there.
+     */
+    @Query("select count(*) from ArticObject where nid = :id limit 1")
+    fun getObjectCountWithId(id: String): Int
 
     /**
      * Return the object with an audio file given by this number.
@@ -52,4 +60,14 @@ interface ArticObjectDao {
 
     @Query("select * from ArticObject where floor = :floor")
     fun getObjectsByFloor(floor: Int): Flowable<List<ArticObject>>
+}
+
+
+/**
+ * Proxy to [ArticObjectDao.getObjectCountWithId]. Returns true if
+ * we have an object in the database under that id, false otherwise.
+ */
+@WorkerThread
+fun ArticObjectDao.hasObjectWithId(id: String?) : Boolean {
+    return id != null && getObjectCountWithId(id) > 0
 }
