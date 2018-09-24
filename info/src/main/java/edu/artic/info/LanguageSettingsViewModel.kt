2c83bc157f58794a486343b3e6970f71ed352a07
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-24 09:35:43 -0400

    [Analytics] - Adds language selected analytics event

--- info/src/main/java/edu/artic/info/LanguageSettingsViewModel.kt
+++ info/src/main/java/edu/artic/info/LanguageSettingsViewModel.kt
@@ -1,9 +1,12 @@
 package edu.artic.info
 
 import com.fuzz.rx.disposedBy
+import edu.artic.analytics.AnalyticsAction
+import edu.artic.analytics.AnalyticsTracker
+import edu.artic.analytics.EventCategoryName
 import edu.artic.localization.LanguageSelector
-import edu.artic.localization.LocalizationPreferences
 import edu.artic.localization.SPANISH
+import edu.artic.localization.nameOfLanguageForAnalytics
 import edu.artic.viewmodel.BaseViewModel
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
@@ -14,7 +17,8 @@ import javax.inject.Inject
  * @author Sameer Dhakal (Fuzz)
  */
 class LanguageSettingsViewModel @Inject constructor(
-        val languageSelector: LanguageSelector
+        val languageSelector: LanguageSelector,
+        private val analyticsTracker: AnalyticsTracker
 ) : BaseViewModel() {
 
 
@@ -27,6 +31,11 @@ class LanguageSettingsViewModel @Inject constructor(
         selectedLanguage
                 .distinctUntilChanged()
                 .subscribe {
+                    analyticsTracker.reportEvent(
+                            EventCategoryName.Language,
+                            AnalyticsAction.languageSelected,
+                            it.nameOfLanguageForAnalytics()
+                    )
                     languageSelector.setDefaultLanguageForApplication(it)
 
                 }.disposedBy(disposeBag)
