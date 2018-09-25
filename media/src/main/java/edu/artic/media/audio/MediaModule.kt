8f97e4b1b99ded62eb926c7ee71f5ae551305f4f
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-25 14:09:06 -0400

    [Audio Tutorial] - Service Emits event when user attempts to audio translation for first time

--- media/src/main/java/edu/artic/media/audio/MediaModule.kt
+++ media/src/main/java/edu/artic/media/audio/MediaModule.kt
@@ -1,7 +1,10 @@
 package edu.artic.media.audio
 
+import android.content.Context
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import javax.inject.Singleton
 
 /**
  * @author Sameer Dhakal (Fuzz)
@@ -12,4 +15,18 @@ abstract class MediaModule {
     @get:ContributesAndroidInjector
     abstract val audioPlayerService: AudioPlayerService
 
+    @Module
+    companion object {
+
+        @JvmStatic
+        @Provides
+        fun provideAudioPrefManager(context: Context):
+                AudioPrefManager = AudioPrefManager(context)
+
+        @JvmStatic
+        @Provides
+        @Singleton
+        fun provideAudioServiceHook(): AudioServiceHook = AudioServiceHook()
+
+    }
 }
