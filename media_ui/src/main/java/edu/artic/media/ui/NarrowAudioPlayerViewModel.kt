b5973c35119aab58160db070ac25d7e9e0a6f476
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-09-25 14:10:04 -0400

    [Audio tutorial] - Displays the audio tutorial when required, Resumes the playback when user views tutorial

--- media_ui/src/main/java/edu/artic/media/ui/NarrowAudioPlayerViewModel.kt
+++ media_ui/src/main/java/edu/artic/media/ui/NarrowAudioPlayerViewModel.kt
@@ -1,10 +1,43 @@
 package edu.artic.media.ui
 
-import edu.artic.viewmodel.BaseViewModel
+import com.fuzz.rx.disposedBy
+import edu.artic.media.audio.AudioPrefManager
+import edu.artic.media.audio.AudioServiceHook
+import edu.artic.viewmodel.NavViewViewModel
+import edu.artic.viewmodel.Navigate
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
 /**
 @author Sameer Dhakal (Fuzz)
  */
-class NarrowAudioPlayerViewModel @Inject constructor() : BaseViewModel() {
+class NarrowAudioPlayerViewModel @Inject constructor(
+        audioServiceHook: AudioServiceHook,
+        private val audioPrefManager: AudioPrefManager
+) : NavViewViewModel<NarrowAudioPlayerViewModel.NavigationEndpoint>() {
+
+    /**
+     * Used to resume the audio player after user reads the audio tutorial.
+     */
+    val resumeAudioPlayBack: Subject<Boolean> = PublishSubject.create()
+
+    fun userSawAudioTutorial() {
+        audioPrefManager.hasPlayedAudioBefore = true
+        resumeAudioPlayBack.onNext(true)
+    }
+
+    sealed class NavigationEndpoint {
+        object AudioTutorial : NavigationEndpoint()
+    }
+
+
+    init {
+        audioServiceHook.playbackStartedForFirstTime
+                .filter { it }
+                .subscribe {
+                    navigateTo.onNext(Navigate.Forward(NavigationEndpoint.AudioTutorial))
+                }.disposedBy(disposeBag)
+    }
+
 }
