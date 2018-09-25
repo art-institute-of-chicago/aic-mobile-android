1d6f8a7220415b38b982270beda91ae65cd53851
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-09-25 15:36:52 -0400

    [Documentation] - Search: explain some basic assumptions about SearchFragment

--- search/src/main/java/edu/artic/search/SearchActivity.kt
+++ search/src/main/java/edu/artic/search/SearchActivity.kt
@@ -13,6 +13,12 @@ import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.search_app_bar_layout.view.*
 import kotlin.reflect.KClass
 
+/**
+ * Try to avoid using this class. Most callers should be fine with adding a
+ * [SearchFragment] to a pre-existing activity.
+ *
+ * Launched by `edu.artic.navigation.NavigationConstants.SEARCH`.
+ */
 class SearchActivity : BaseViewModelActivity<SearchViewModel>() {
     override val viewModelClass: KClass<SearchViewModel>
         get() = SearchViewModel::class
