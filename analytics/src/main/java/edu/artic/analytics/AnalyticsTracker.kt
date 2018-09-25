af3479cf82ab623f67121b79f7eefc1094b13c9e
caguilar187 <caguilar187@gmail.com>
2018-09-25 13:08:58 -0400

    [AIC-116] adds custom dimensions missing onsite one

--- analytics/src/main/java/edu/artic/analytics/AnalyticsTracker.kt
+++ analytics/src/main/java/edu/artic/analytics/AnalyticsTracker.kt
@@ -3,6 +3,10 @@ package edu.artic.analytics
 import android.content.Context
 import com.google.android.gms.analytics.GoogleAnalytics
 import com.google.android.gms.analytics.HitBuilders
+import edu.artic.localization.LanguageSelector
+import edu.artic.localization.nameOfLanguageForAnalytics
+import edu.artic.membership.MemberInfoPreferencesManager
+import java.util.*
 
 /**
  * Description:
@@ -23,23 +27,33 @@ interface AnalyticsTracker {
 }
 
 class AnalyticsTrackerImpl(context: Context,
+                           private val languageSelector: LanguageSelector,
+                           private val membershipPrefs: MemberInfoPreferencesManager,
                            private val analyticsConfig: AnalyticsConfig) : AnalyticsTracker {
 
     private val analytics = GoogleAnalytics.getInstance(context)
     private val tracker = analytics.newTracker(analyticsConfig.trackingId)
 
     override fun reportEvent(category: String, action: String, label: String) {
+        val memberId = membershipPrefs.memberID
         tracker.send(HitBuilders.EventBuilder()
                 .setCategory(category)
                 .setAction(action)
+                .setCustomDimension(2, (if (memberId != null) "Member" else "None"))
+                .setCustomDimension(3, languageSelector.getAppLocale().nameOfLanguageForAnalytics())
+                .setCustomDimension(4, Locale.getDefault().toString())
                 .setLabel(label).build())
     }
 
 
     override fun reportScreenView(name: String) {
+        val memberId = membershipPrefs.memberID
         tracker.apply {
             setScreenName(name)
             send(HitBuilders.ScreenViewBuilder()
+                    .setCustomDimension(2, (if (memberId != null) "Member" else "None"))
+                    .setCustomDimension(3, languageSelector.getAppLocale().nameOfLanguageForAnalytics())
+                    .setCustomDimension(4, Locale.getDefault().toString())
                     .build())
         }
     }
