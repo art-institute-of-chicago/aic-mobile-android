061c928a611b59dcde1efa4b52df3465107bf3c8
caguilar187 <caguilar187@gmail.com>
2018-09-28 18:11:04 -0400

    use default on asset map tiler

--- map/src/main/kotlin/edu/artic/map/rendering/AssetMapTileProvider.kt
+++ map/src/main/kotlin/edu/artic/map/rendering/AssetMapTileProvider.kt
@@ -2,7 +2,9 @@ package edu.artic.map.rendering
 
 import android.content.Context
 import android.graphics.Bitmap
-import com.bumptech.glide.Glide
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
 import com.google.android.gms.maps.model.Tile
 import edu.artic.db.models.ArticMapFloor
 import timber.log.Timber
@@ -10,11 +12,19 @@ import java.io.ByteArrayOutputStream
 import java.io.IOException
 import kotlin.math.pow
 
+
+
+
 /**
  * Description:
  */
 class AssetMapTileProvider(private val context: Context,
                            private val floor: ArticMapFloor) : BaseMapTileProvider() {
+
+    val defaultTile = Tile(
+            TILE_SIZE.toInt(),
+            TILE_SIZE.toInt(),
+            context.assets.open("tiles/blank-tile.jpg").readBytes())
 
     override fun getAdjustedTile(x: Int, y: Int, zoom: Int): Tile? {
         return when {
@@ -28,10 +38,20 @@ class AssetMapTileProvider(private val context: Context,
 
                 val path = "floor${floor.number}/zoom$zoom/tiles-$tileNumber.jpg"
                 return try {
-                    val array = context.assets.open(path).readBytes()
+                    val stream = context.assets.open(path)
+                    val bitmap = BitmapFactory.decodeStream(stream)
+                    val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+                    val canvas = Canvas(newBitmap)
+                    val alphaPaint = Paint()
+                    alphaPaint.alpha = 40
+                    canvas.drawBitmap(bitmap, 0f, 0f, alphaPaint)
+                    val outStream = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
+                    val byteArray = outStream.toByteArray()
+                    bitmap.recycle()
                     Tile(TILE_SIZE.toInt(),
                             TILE_SIZE.toInt(),
-                            array
+                            byteArray
                     ).also {
                         Timber.d("Loaded tile with path $path")
                     }
@@ -41,7 +61,7 @@ class AssetMapTileProvider(private val context: Context,
                 }
             }
             else -> {
-                null
+                defaultTile
             }
         }
     }
