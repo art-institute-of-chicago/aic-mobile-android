ead4f6ed8162a7dd5db24d7db5b5cd75337374b0
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-10-01 13:15:29 -0400

    [Refactor] - L10n: Use 310-style date-time parsers over string constants

--- base/src/main/java/edu/artic/base/utils/DateTimeHelper.kt
+++ base/src/main/java/edu/artic/base/utils/DateTimeHelper.kt
@@ -4,6 +4,9 @@ import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.format.DateTimeFormatterBuilder
+import org.threeten.bp.format.SignStyle
+import org.threeten.bp.format.TextStyle
+import org.threeten.bp.temporal.ChronoField.*
 
 /**
 @author Sameer Dhakal (Fuzz)
@@ -11,22 +14,31 @@ import org.threeten.bp.format.DateTimeFormatterBuilder
 
 class DateTimeHelper {
     companion object {
-        const val DEFAULT_FORMAT: String = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
 
-        val DEFAULT_FORMATTER: DateTimeFormatter = DateTimeFormatterBuilder()
-                .appendPattern(DEFAULT_FORMAT)
-                .toFormatter()
+        val DEFAULT_FORMATTER: DateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME
 
         val MONTH_DAY_FORMATTER: DateTimeFormatter = DateTimeFormatterBuilder()
-                .appendPattern("MMMM d")
+                .appendText(MONTH_OF_YEAR, TextStyle.FULL)
+                .appendLiteral(' ')
+                .appendValue(DAY_OF_MONTH)
                 .toFormatter()
 
         val HOME_EXHIBITION_DATE_FORMATTER: DateTimeFormatter = DateTimeFormatterBuilder()
-                .appendPattern("MMMM d, yyyy")
+                .appendText(MONTH_OF_YEAR, TextStyle.FULL)
+                .appendLiteral(' ')
+                .appendValue(DAY_OF_MONTH)
+                .appendLiteral(", ")
+                .appendValue(YEAR, 4)
                 .toFormatter()
 
         val HOME_EVENT_DATE_FORMATTER: DateTimeFormatter = DateTimeFormatterBuilder()
-                .appendPattern("MMMM d   h:mm a")
+                .append(MONTH_DAY_FORMATTER)
+                .appendLiteral("   ")
+                .appendValue(CLOCK_HOUR_OF_AMPM, 1, 2, SignStyle.NORMAL)
+                .appendLiteral(':')
+                .appendValue(MINUTE_OF_HOUR, 2)
+                .appendLiteral(' ')
+                .appendText(AMPM_OF_DAY)
                 .toFormatter()
     }
 }
