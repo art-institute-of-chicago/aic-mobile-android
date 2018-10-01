c3bd66c2f783c2b015eeded03a87214d6a4fad61
caguilar187 <caguilar187@gmail.com>
2018-10-01 13:56:44 -0400

    [Docs] documenting tiler.sh

--- map/tiler.sh
+++ map/tiler.sh
@@ -5,35 +5,32 @@ generateZoomLevels(){
   ZOOM_LEVEL="$2"
   FLOOR="$3"
 
-  SIZE=$((512*(2**$ZOOM_LEVEL)))
+  # 512px is the size of a singular tile
+  # generating the size of the image we need to the power of 2 of the and current zoom level
+  OUTPUT_SIZE=$((512*(2**$ZOOM_LEVEL)))
 
-  # should the scale value (currently 0.905) be played with modifcations need to
-  # also occur in $LEFT_CHOP_AMOUNT_SCALED
-  SCALED_WIDTH=$(bc <<< "scale=0; (${SIZE}*0.905)/1")
+  # generate the full SIZE image background
+  magick convert -size "${OUTPUT_SIZE}x${OUTPUT_SIZE}" xc:"rgb(82,122,157)" tileBg.jpg
 
-  # should the scale value (currently 0.8974) be played with modifcations need to
-  # also occur in $VERTICAL_OFFSET_MODIFIER
-  SCALED_HEIGHT=$(bc <<< "scale=0; (${SIZE}*0.8974)/1")
+  # calculated width of the image from the pdf to be used to cover the museum bounds within the Tile matrix
+  # this value is scaled from the OUTPUT_SIZE (currently 0.905)
+  SCALED_WIDTH=$(bc <<< "scale=0; (${OUTPUT_SIZE}*0.905)/1")
 
-  PAD_AMOUNT_SCALED=$((1*(2**${ZOOM_LEVEL})))
-  echo $PAD_AMOUNT_SCALED
+  # calculated height of the image from the pdf to be used to cover the museum bounds within the Tile matrix
+  # this value is scaled from the OUTPUT_SIZE (currently 0.8974)
+  SCALED_HEIGHT=$(bc <<< "scale=0; (${OUTPUT_SIZE}*0.8974)/1")
 
-  # generated value for use in removing that amount from the scaledAndRotated.jpg image
-  LEFT_CHOP_AMOUNT_SCALED=$(((30*(2**${ZOOM_LEVEL}))+${PAD_AMOUNT_SCALED}))
+  #generate a scaled down and rotated by -1 degree image from PDF
+  magick convert -density 300 $FILENAME -scale "${SCALED_WIDTH}x${SCALED_HEIGHT}!" -background "rgb(82,122,157)" -rotate "-1" scaledAndRotated.jpg
 
-  #generate a scaled scaled down and rotated by -1 degree image from PDF
-  #echo "magick convert -density 300 $FILENAME -scale \"${SCALED_WIDTH}x${SCALED_HEIGHT}\" -rotate \"-1.5\" -background transparent scaledAndRotated.jpg"
-  magick convert -density 300 $FILENAME -scale "${SCALED_WIDTH}x${SCALED_HEIGHT}!" -background "rgb(82,122,157)" -rotate "-1" scaledAndRotated.jpg 
+  # chop off a portion of the image to push the image down into it's correct position on Google Maps
+  # The left side is to the west on Google Maps and will be flush with the full scale image at OUTPUT_SIZE
+  LEFT_CHOP_AMOUNT_SCALED=$(((31*(2**${ZOOM_LEVEL}))))
+  magick convert scaledAndRotated.jpg -chop "${LEFT_CHOP_AMOUNT_SCALED}x0" chopped.jpg
 
-  #removes a dynamically checked cut
-  magick convert scaledAndRotated.jpg -background "rgb(82,122,157)" -splice "0x${PAD_AMOUNT_SCALED}" -gravity east -background "rgb(82,122,157)" -splice "${PAD_AMOUNT_SCALED}x0" padded.jpg
-  magick convert padded.jpg -chop "${LEFT_CHOP_AMOUNT_SCALED}x0" -gravity south -chop "0x${PAD_AMOUNT_SCALED}" chopped.jpg
-  magick convert -size "${SIZE}x${SIZE}" xc:"rgb(82,122,157)" tileBg.jpg
-
-  VERTICAL_OFFSET_MODIFIER=$((33*(2**$ZOOM_LEVEL)))
-
-  # sets the scaled and cropped image into as close a place as possible to
-  # where it should be on google's map
+  # composite the previously chopped image on to the generated tileBg image
+  # the chopped image is offset vertically by this offset to adjust for the north/south offset
+  VERTICAL_OFFSET_MODIFIER=$((34*(2**$ZOOM_LEVEL)))
   magick composite -gravity northWest -geometry "+0+${VERTICAL_OFFSET_MODIFIER}" chopped.jpg tileBg.jpg tile.jpg
 
   # create directory if not already created
@@ -44,10 +41,14 @@ generateZoomLevels(){
   echo "zoome level: $ZOOM_LEVEL tiles generated for floor $FLOOR"
 }
 
+# generate the various zoom levels used by the app
+# pass in the pdf file, the request zoom level and floor number
 # zoom level 1 does not seem to be used at this point
 # generateZoomLevels "$1" "1" "$2"
 generateZoomLevels "$1" "2" "$2"
 generateZoomLevels "$1" "3" "$2"
 generateZoomLevels "$1" "4" "$2"
 generateZoomLevels "$1" "5" "$2"
+# we only support these four zoom levels as we max it out, but you could add more below
+# additionally each new zoom level takes twice as long to run 5 currently takes about 10 to 15 minutes
 # generateZoomLevels "$1" "6" "$2"
