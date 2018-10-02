8d8f8dd91dc86b66f4103052da5eceb6a690c114
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-10-02 14:59:23 -0400

    [ViewModel] - Dispose of CellViewModels together with their adapters

--- viewmodel/src/main/kotlin/edu/artic/viewmodel/CellViewModel.kt
+++ viewmodel/src/main/kotlin/edu/artic/viewmodel/CellViewModel.kt
@@ -1,5 +1,7 @@
 package edu.artic.viewmodel
 
+import com.fuzz.rx.DisposeBag
+
 /**
  * Specialized [BaseViewModel] designed for use with the ':adapter` module's
  * `edu.artic.adapter.AutoHolderRecyclerViewAdapter`.
@@ -8,12 +10,22 @@ package edu.artic.viewmodel
  *
  * These need to have a longer lifecycle than [viewDisposeBag] but shorter than
  * [BaseViewModelFragment's][BaseViewModelFragment.disposeBag] or
- * [BaseViewModelActivity's][BaseViewModelActivity.disposeBag].
+ * [BaseViewModelActivity's][BaseViewModelActivity.disposeBag]. Thus, the precise
+ * lifecycle can be passed in as the `whatDisposesAdapter` constructor
+ * parameter.
+ *
+ * You may pass `null` in for `whatDisposesAdapter` if this object's
+ * [disposeBag] is never used.
+ *
+ * Note: this mechanism will not invoke [onClearedListener] (at least not of writing).
  *
  * @author Philip Cohn-Cort (Fuzz)
+ * @param whatDisposesAdapter this should be the same [DisposeBag] that is
+ * responsible for cleaning up subscriptions on the associated adapter.
  */
-abstract class CellViewModel() : BaseViewModel() {
+abstract class CellViewModel(whatDisposesAdapter: DisposeBag?) : BaseViewModel() {
 
     init {
+        whatDisposesAdapter?.add(disposeBag)
     }
 }
