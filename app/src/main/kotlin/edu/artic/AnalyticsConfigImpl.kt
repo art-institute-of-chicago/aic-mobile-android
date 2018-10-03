9c606094fbc85a15efd944fd04dffd2243e32363
caguilar187 <caguilar187@gmail.com>
2018-10-03 12:02:16 -0400

    [AIC-260] add support for crashlytics and load variables from properties files

--- app/src/main/kotlin/edu/artic/AnalyticsConfigImpl.kt
+++ app/src/main/kotlin/edu/artic/AnalyticsConfigImpl.kt
@@ -9,11 +9,6 @@ class AnalyticsConfigImpl : AnalyticsConfig {
     override val enabled: Boolean
         get() = true
 
-    /**
-     * TODO:: update real tracking id
-     * Uses dev account for development purpose.
-     * Dev account is registered under android@fuzzproductions.com
-     */
     override val trackingId: String
-        get() = "UA-122937378-1"
+        get() = BuildConfig.GA_TRACKING_ID
 }
