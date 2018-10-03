903bb4ec5bec54357a1764888fec9c380604321b
caguilar187 <caguilar187@gmail.com>
2018-10-03 15:48:05 -0400

    [AIC-522] fixing compass orientation issues

--- map/src/main/kotlin/edu/artic/map/MapFragment.kt
+++ map/src/main/kotlin/edu/artic/map/MapFragment.kt
@@ -456,7 +456,8 @@ class MapFragment : BaseViewModelFragment<MapViewModel>() {
                     if (location != null) {
                         compass.rotation = 0.0f
                         compass.alpha = 1.0f
-                        map.uiSettings.setAllGesturesEnabled(false)
+                        map.uiSettings.isRotateGesturesEnabled = false
+                        map.uiSettings.isScrollGesturesEnabled = false
 
                         map.animateCamera(
                                 CameraUpdateFactory
@@ -468,7 +469,8 @@ class MapFragment : BaseViewModelFragment<MapViewModel>() {
                                         )
                         )
                     } else {
-                        map.uiSettings.setAllGesturesEnabled(true)
+                        map.uiSettings.isRotateGesturesEnabled = true
+                        map.uiSettings.isScrollGesturesEnabled = true
                         compass.rotation = 30f
                         compass.alpha = .5f
                     }
