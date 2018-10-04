1fc9d66d0f3ff425a668fab3eb4b98fe0bcf6180
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-10-04 09:49:08 -0400

    [Image] - Adds placeholder image for welcome screen (tour, events & exhibition cells)

--- welcome/src/main/kotlin/edu/artic/welcome/WelcomeEventsAdapter.kt
+++ welcome/src/main/kotlin/edu/artic/welcome/WelcomeEventsAdapter.kt
@@ -1,12 +1,12 @@
 package edu.artic.welcome
 
 import android.view.View
-import com.bumptech.glide.Glide
 import com.fuzz.rx.bindToMain
 import com.fuzz.rx.disposedBy
 import com.jakewharton.rxbinding2.widget.text
 import edu.artic.adapter.AutoHolderRecyclerViewAdapter
 import edu.artic.adapter.BaseViewHolder
+import edu.artic.image.GlideApp
 import kotlinx.android.synthetic.main.welcome_event_cell_layout.view.*
 
 
@@ -36,8 +36,9 @@ class WelcomeEventsAdapter : AutoHolderRecyclerViewAdapter<WelcomeEventCellViewM
         item.eventImageUrl
                 .filter { it.isNotEmpty() }
                 .subscribe {
-                    Glide.with(context)
+                    GlideApp.with(context)
                             .load(it)
+                            .placeholder(R.drawable.rect_default_image)
                             .into(image)
                 }.disposedBy(item.viewDisposeBag)
 
--- welcome/src/main/kotlin/edu/artic/welcome/WelcomeExhibitionAdapter.kt
+++ welcome/src/main/kotlin/edu/artic/welcome/WelcomeExhibitionAdapter.kt
@@ -1,12 +1,12 @@
 package edu.artic.welcome
 
 import android.view.View
-import com.bumptech.glide.Glide
 import com.fuzz.rx.bindToMain
 import com.fuzz.rx.disposedBy
 import com.jakewharton.rxbinding2.widget.text
 import edu.artic.adapter.AutoHolderRecyclerViewAdapter
 import edu.artic.adapter.BaseViewHolder
+import edu.artic.image.GlideApp
 import kotlinx.android.synthetic.main.welcome_on_view_cell_layout.view.*
 
 /**
@@ -31,8 +31,9 @@ class OnViewAdapter : AutoHolderRecyclerViewAdapter<WelcomeExhibitionCellViewMod
         item.exhibitionImageUrl
                 .filter { it.isNotEmpty() }
                 .subscribe {
-                    Glide.with(context)
+                    GlideApp.with(context)
                             .load(it)
+                            .placeholder(R.drawable.square_default_image)
                             .into(image)
                 }.disposedBy(item.viewDisposeBag)
 
