50a054f409525a165e83fe5d6a45643763033e05
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-10-04 12:07:18 -0400

    [Welcome] - Adds placholder image in see all pages

--- events/src/main/kotlin/edu/artic/events/AllEventsAdapter.kt
+++ events/src/main/kotlin/edu/artic/events/AllEventsAdapter.kt
@@ -1,12 +1,12 @@
 package edu.artic.events
 
 import android.view.View
-import com.bumptech.glide.Glide
 import com.fuzz.rx.bindToMain
 import com.fuzz.rx.disposedBy
 import com.jakewharton.rxbinding2.widget.text
 import edu.artic.adapter.AutoHolderRecyclerViewAdapter
 import edu.artic.adapter.BaseViewHolder
+import edu.artic.image.GlideApp
 import kotlinx.android.synthetic.main.cell_all_events_layout.view.*
 
 /**
@@ -27,8 +27,9 @@ class AllEventsAdapter : AutoHolderRecyclerViewAdapter<AllEventsCellBaseViewMode
             headerText.visibility = View.GONE
 
             item.eventImageUrl.subscribe {
-                Glide.with(context)
+                GlideApp.with(context)
                         .load(it)
+                        .placeholder(R.drawable.thumb_placeholder)
                         .into(image)
             }.disposedBy(item.viewDisposeBag)
 
