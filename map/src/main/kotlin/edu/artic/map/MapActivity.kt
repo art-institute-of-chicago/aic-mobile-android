7dfa1e219b27773461899c864a82ed7938719e85
caguilar187 <caguilar187@gmail.com>
2018-10-16 12:31:10 -0400

    [AIC-557] maintain the map session till the user clicks back on welcomeactivity

--- map/src/main/kotlin/edu/artic/map/MapActivity.kt
+++ map/src/main/kotlin/edu/artic/map/MapActivity.kt
@@ -1,15 +1,18 @@
 package edu.artic.map
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.MemoryCategory
+import edu.artic.base.utils.asDeepLinkIntent
 import edu.artic.base.utils.disableShiftMode
 import edu.artic.base.utils.preventReselection
 import edu.artic.location.LocationPreferenceManager
 import edu.artic.map.tutorial.TutorialPreferencesManager
+import edu.artic.navigation.NavigationConstants
 import edu.artic.navigation.NavigationSelectListener
 import edu.artic.ui.BaseActivity
 import kotlinx.android.synthetic.main.activity_map.*
@@ -85,4 +88,15 @@ class MapActivity : BaseActivity() {
         Glide.get(this).setMemoryCategory(MemoryCategory.NORMAL)
     }
 
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            if (navController.currentDestination?.id == R.id.mapFragment) {
+                val intent = NavigationConstants.HOME.asDeepLinkIntent()
+                intent.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT or Intent.FLAG_ACTIVITY_NO_ANIMATION
+                startActivity(intent)
+                return
+            }
+        }
+        super.onBackPressed()
+    }
 }
