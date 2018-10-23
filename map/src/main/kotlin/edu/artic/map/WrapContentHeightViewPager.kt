3a2d5de65e53caa4020db5513a984f64dbaafa3f
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-10-23 18:05:29 -0400

    [Refactor] - Search : Considers viewHolder's padding while computing the height

--- map/src/main/kotlin/edu/artic/map/WrapContentHeightViewPager.kt
+++ map/src/main/kotlin/edu/artic/map/WrapContentHeightViewPager.kt
@@ -3,6 +3,7 @@ package edu.artic.map
 import android.content.Context
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
+import android.view.View
 
 
 /**
@@ -12,17 +13,21 @@ import android.util.AttributeSet
  */
 class WrapContentHeightViewPager(context: Context, attrs: AttributeSet? = null) : ViewPager(context, attrs) {
 
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val newHeightMeasureSpec: Int
+        var newHeightMeasureSpec = heightMeasureSpec
+
         var height = 0
         for (i in 0 until childCount) {
             val child = getChildAt(i)
-            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
+            child.measure(widthMeasureSpec, View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
             val h = child.measuredHeight
             if (h > height) height = h
         }
 
-        newHeightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+        if (height != 0) {
+            newHeightMeasureSpec = View.MeasureSpec.makeMeasureSpec(height + paddingTop + paddingBottom, View.MeasureSpec.EXACTLY)
+        }
 
         super.onMeasure(widthMeasureSpec, newHeightMeasureSpec)
     }
