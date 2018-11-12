7ef0f2d205a7de416888a36d40f2816aab8db92c
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-11-12 17:45:35 -0500

    [Documentation] - Explain purpose of DBModule

--- db/src/main/kotlin/edu/artic/db/DBModule.kt
+++ db/src/main/kotlin/edu/artic/db/DBModule.kt
@@ -6,6 +6,23 @@ import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
+/**
+ * This dagger2 module allows access to each of the
+ * [DAOs][android.arch.persistence.room.Dao] in the application.
+ *
+ * Here, one DAO usually represents exactly one type of persisted
+ * model - so there's an [ArticObjectDao][edu.artic.db.daos.ArticObjectDao]
+ * for [ArticObjects][edu.artic.db.models.ArticObject], a
+ * [ArticGalleryDao][edu.artic.db.daos.ArticGalleryDao] for
+ * [ArticGalleries][edu.artic.db.models.ArticGallery], and so forth.
+ *
+ * Of special note is [provideDB] - this returns a singleton
+ * [AppDatabase], within which is stored every other DAO offered in
+ * this file. We broadly recommend against injecting the entire
+ * database into other parts of the application.
+ *
+ * @see AppDataManager
+ */
 @Module
 class DBModule {
 
