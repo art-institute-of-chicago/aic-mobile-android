3413815695f5c673fc5c4e739994142070db84a0
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-11-13 10:01:41 -0500

    [Refactor] - Exception : Adds new base exception class `PermissibleException`, makes NetworkException permissive

--- base/src/main/java/edu/artic/base/NetworkException.kt
+++ base/src/main/java/edu/artic/base/NetworkException.kt
@@ -4,6 +4,18 @@ package edu.artic.base
  * @author Sameer Dhakal (Fuzz)
  */
 class NetworkException(
-        override var message: String,
-        override var cause: Throwable? = null
-) : Exception(message, cause)
+        override var message: String? = null,
+        override var cause: Throwable
+) : PermissibleException(message, cause)
+
+/**
+ * Exceptions that do not create the app in an unstable state.
+ * E.g. Network exceptions are PermissibleException.
+ * If this exception is encountered during data loading process, user is allowed to use
+ * the app (i.e. app is not stuck at splash screen). All other exceptions are not permitted.
+ *
+ * @see [edu.artic.splash.SplashActivity]
+ */
+open class PermissibleException(
+        override var message: String? = null,
+        override var cause: Throwable) : Exception(message, cause)
