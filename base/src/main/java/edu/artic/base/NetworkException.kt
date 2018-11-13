f25f67ddbb81847dfb5b85dd388240005aa5e8ea
Sameer Dhakal <sdhakal@fuzzproductions.com>
2018-11-13 12:57:34 -0500

    [Refactor] - DB : Renames `PermissibleException` ->  `PermissibleError`

--- base/src/main/java/edu/artic/base/NetworkException.kt
+++ base/src/main/java/edu/artic/base/NetworkException.kt
@@ -4,18 +4,18 @@ package edu.artic.base
  * @author Sameer Dhakal (Fuzz)
  */
 class NetworkException(
-        override var message: String? = null,
-        override var cause: Throwable
-) : PermissibleException(message, cause)
+        override var message: String,
+        override var cause: Throwable? = null
+) : PermissibleError(message, cause)
 
 /**
  * Exceptions that do not create the app in an unstable state.
- * E.g. Network exceptions are PermissibleException.
+ * E.g. Network exceptions are PermissibleError.
  * If this exception is encountered during data loading process, user is allowed to use
  * the app (i.e. app is not stuck at splash screen). All other exceptions are not permitted.
  *
  * @see [edu.artic.splash.SplashActivity]
  */
-open class PermissibleException(
-        override var message: String? = null,
-        override var cause: Throwable) : Exception(message, cause)
+open class PermissibleError(
+        override var message: String,
+        override var cause: Throwable? = null) : Exception(message, cause)
