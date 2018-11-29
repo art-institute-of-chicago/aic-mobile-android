fbab2c7ce1525d20cb29c25def89f68d60e8e489
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2018-11-29 16:04:18 -0500

    [Util] - Delete unused and obsolete code in UrlExtension.kt
    NB: pre-existing logic with Uri::parse and Uri::getAuthority did
    not perform any validation, and so we lose nothing by deleting
    it.

--- db/src/main/kotlin/edu/artic/ui/util/UrlExtension.kt
+++ db/src/main/kotlin/edu/artic/ui/util/UrlExtension.kt
@@ -1,8 +1,5 @@
 package edu.artic.ui.util
 
-import android.net.Uri
-import edu.artic.db.models.ArticDataObject
-
 /**
  * Use this to ensure that your image urls are always hitting the CDN.
  *
@@ -13,38 +10,5 @@ import edu.artic.db.models.ArticDataObject
         replaceWith = ReplaceWith("toString()")
 )
 fun String.asCDNUri() : String {
-    val parsed = Uri.parse(this)
-    return when {
-        parsed.authority == ArticDataObject.IMAGE_SERVER_URL -> parsed.buildUpon()
-                .authority(ArticDataObject.IMAGE_SERVER_URL)
-                .build()
-        else -> parsed
-    }.toString()
-}
-
-/**
- * Returns this if it is not null, or else [Uri.EMPTY] if it _is_ `null`.
- *
- * This method is inspired by [String.orEmpty].
- */
-inline fun Uri?.orEmpty(): Uri {
-    return this ?: Uri.EMPTY
-}
-
-/**
- * Checks whether this is `==` to [Uri.EMPTY].
- *
- * This method is inspired by [String.isEmpty].
- */
-inline fun Uri.isEmpty(): Boolean {
-    return this == Uri.EMPTY
-}
-
-/**
- * Checks whether this is `!=` to [Uri.EMPTY].
- *
- * This method is inspired by [String.isNotEmpty].
- */
-inline fun Uri.isNotEmpty(): Boolean {
-    return !isEmpty()
+    return this
 }
