27023675c65b5bf72692097e06dcf725d2c7b0b7
nitin <nitinhemmady@gmail.com>
2023-02-22 19:18:29 -0600

    Replace deprecated ViewModelProviders with ViewModelProvider

--- viewmodel/src/main/kotlin/edu/artic/viewmodel/ViewModelExtensions.kt
+++ viewmodel/src/main/kotlin/edu/artic/viewmodel/ViewModelExtensions.kt
@@ -1,21 +1,20 @@
 package edu.artic.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 
 /**
  * Description:
  */
 fun androidx.fragment.app.FragmentActivity.viewModelProvider(
     useFactory: Boolean,
-    viewModelFactory: ViewModelFactory
+    viewModelFactory: ViewModelFactory,
 ): ViewModelProvider = when {
-    useFactory -> ViewModelProviders.of(this, viewModelFactory)
-    else -> ViewModelProviders.of(this)
+    useFactory -> ViewModelProvider(this, viewModelFactory)
+    else -> ViewModelProvider(this)
 }
 
 fun BaseViewModelFragment<*, *>.getViewModelProvider(viewModelFactory: ViewModelFactory): ViewModelProvider =
     when {
-        this.useFactory -> ViewModelProviders.of(this, viewModelFactory)
-        else -> ViewModelProviders.of(this)
+        this.useFactory -> ViewModelProvider(this, viewModelFactory)
+        else -> ViewModelProvider(this)
     }
